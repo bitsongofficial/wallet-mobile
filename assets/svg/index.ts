4b92520b617e1b320cc38768ba3c1cd07ca56453
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-04-27 19:02:42 +0300

    feat(assets): 🏞 add .svg

---
+++ assets/svg/index.ts
@@ -0,0 +1,26 @@
+// @ts-nocheck
+import arrow_down from "./arrow_down.svg";
+import arrow_up_border from "./arrow_up_border.svg";
+import arrow_up from "./arrow_up.svg";
+import bell from "./bell.svg";
+import home from "./home.svg";
+import meatballs from "./meatballs.svg";
+import qr_code from "./qr_code.svg";
+import stake from "./stake.svg";
+import target from "./target.svg";
+import tip from "./tip.svg";
+import unstake from "./unstake.svg";
+
+export default {
+  arrow_down,
+  arrow_up_border,
+  arrow_up,
+  bell,
+  home,
+  meatballs,
+  qr_code,
+  stake,
+  target,
+  tip,
+  unstake,
+};
