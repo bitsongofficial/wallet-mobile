5c44ee984236c32d09b3d7724daf2ecc67ec11e8
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-04-28 18:09:35 +0300

    chore: 🔧 upd. Icon as placeholder
    svg assembly not configured

--- components/atoms/Icon.tsx
+++ components/atoms/Icon.tsx
@@ -2,22 +2,40 @@ import React from "react";
 import { SvgCss, XmlProps } from "react-native-svg";
 // import {COLOR} from '../../utils';
 import icons from "assets/svg";
+import { StyleSheet, View } from "react-native";
 
-type Props = Omit<XmlProps, "xml"> & {
-  size: number;
+type Props = Omit<XmlProps, "xml" | "fill"> & {
+  size?: number;
+  fill?: string;
   name: keyof typeof icons;
 };
-export default ({ size, name, fill, ...props }: Props) => {
+export default ({ size = 14, name, fill, ...props }: Props) => {
   const style = {
     width: size,
     height: size,
   };
   return (
-    <SvgCss
-      {...props}
-      {...style}
-      xml={icons[name]}
-      style={[style, props.style]}
+    <View
+      style={[
+        styles.fake,
+        style,
+        fill !== undefined ? { backgroundColor: fill } : undefined,
+        props.style,
+      ]}
     />
   );
+  // return (
+  //   <SvgCss
+  //     {...props}
+  //     {...style}
+  //     xml={icons[name]}
+  //     style={[style, props.style]}
+  //   />
+  // );
 };
+
+const styles = StyleSheet.create({
+  fake: {
+    backgroundColor: "white",
+  },
+});
