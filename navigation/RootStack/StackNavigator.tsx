3f629e212feac4472c6580f5e6564dcfd1276864
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-04-28 18:12:44 +0300

    refactor: upd. navigaton

---
+++ navigation/RootStack/StackNavigator.tsx
@@ -0,0 +1,48 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BottomTabNavigator from "navigation/BottomTab";
+import * as Screens from "screens";
+import { RootStackParamList } from "types";
+
+/**
+ * A root stack navigator is often used for displaying modals on top of all other content.
+ * https://reactnavigation.org/docs/modal
+ */
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Root"
+      component={BottomTabNavigator}
+      options={{ headerShown: false }}
+    />
+    {/* <Stack.Screen
+        name="AccountHome"
+        component={Screens.AccountHome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="CreateWallet"
+        component={Screens.CreateWallet}
+        options={{ title: 'Create New Mnemonic' }}
+      />
+      <Stack.Screen
+        name="ImportWallet"
+        component={Screens.ImportWallet}
+        options={{ title: 'Import Existing Wallet' }}
+      />
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NotFound"
+        component={Screens.NotFoundScreen}
+        options={{ title: "Oops!" }}
+      />
+      <Stack.Group screenOptions={{ presentation: "modal" }}>
+        <Stack.Screen name="Modal" component={Screens.ModalScreen} />
+      </Stack.Group> */}
+  </Stack.Navigator>
+);
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,145 +3,28 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
 } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
-import { ColorSchemeName, Pressable } from "react-native";
-
-import Colors from "../constants/Colors";
-import useColorScheme from "../hooks/useColorScheme";
-import * as Screens from "../screens";
-import {
-  RootStackParamList,
-  RootTabParamList,
-  RootTabScreenProps,
-} from "../types";
+import { ColorSchemeName } from "react-native";
 import LinkingConfiguration from "./LinkingConfiguration";
+import RootStack from "./RootStack";
 
-export default function Navigation({
-  colorScheme,
-}: {
+type Props = {
   colorScheme: ColorSchemeName;
-}) {
+};
+
+export default function Navigation({ colorScheme }: Props) {
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
+      // TODO: remove
       theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
     >
-      <RootNavigator />
+      <RootStack />
     </NavigationContainer>
   );
-}
-
-/**
- * A root stack navigator is often used for displaying modals on top of all other content.
- * https://reactnavigation.org/docs/modal
- */
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
-function RootNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Main"
-        component={Screens.MainScreen}
-        options={{ headerShown: false }}
-      />
-      {/* <Stack.Screen
-        name="AccountHome"
-        component={Screens.AccountHome}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="CreateWallet"
-        component={Screens.CreateWallet}
-        options={{ title: 'Create New Mnemonic' }}
-      />
-      <Stack.Screen
-        name="ImportWallet"
-        component={Screens.ImportWallet}
-        options={{ title: 'Import Existing Wallet' }}
-      />
-      <Stack.Screen
-        name="Root"
-        component={BottomTabNavigator}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="NotFound"
-        component={Screens.NotFoundScreen}
-        options={{ title: "Oops!" }}
-      />
-      <Stack.Group screenOptions={{ presentation: "modal" }}>
-        <Stack.Screen name="Modal" component={Screens.ModalScreen} />
-      </Stack.Group> */}
-    </Stack.Navigator>
-  );
-}
-
-/**
- * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
- * https://reactnavigation.org/docs/bottom-tab-navigator
- */
-const BottomTab = createBottomTabNavigator<RootTabParamList>();
-
-function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <BottomTab.Navigator
-      initialRouteName="TabOne"
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}
-    >
-      <BottomTab.Screen
-        name="TabOne"
-        component={Screens.TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<"TabOne">) => ({
-          title: "Tab One",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate("Modal")}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}
-            >
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
-        })}
-      />
-      <BottomTab.Screen
-        name="TabTwo"
-        component={Screens.TabTwoScreen}
-        options={{
-          title: "Tab Two",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
-}
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
-  color: string;
-}) {
-  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
 }
