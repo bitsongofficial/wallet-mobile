a72900b3c6fa4500b727a0042a7014d4f68003f2
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-04-29 16:52:04 +0300

    feat(utils): 🔧  added round()

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,7 @@ export function hexAlpha(hex: string, percent: number): string {
   const str = Math.trunc((percent * 255) / 100).toString(16);
   return hex + (str.length === 2 ? str : `0${str}`);
 }
+
+export function round(v: number, num = 2) {
+  return +v.toFixed(num);
+}
