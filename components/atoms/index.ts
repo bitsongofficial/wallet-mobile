92884f543a795dd0e62f78c7ebbc61b388d7e5de
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-03 18:10:54 +0300

    feat: ✨ added wip Card

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Icon } from "./Icon";
 export { default as Button } from "./Button";
 export { default as ButtonBack } from "./ButtonBack";
 export { default as ThemedGradient } from "./ThemedGradient";
+export { default as Card } from "./Card";
