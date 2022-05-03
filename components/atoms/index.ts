7a29928f1e43b482831b5b8fc1a84f6abfcbfafc
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-03 18:09:50 +0300

    feat: ✨ added ButtonBack

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,3 +1,4 @@
 export { default as Icon } from "./Icon";
 export { default as Button } from "./Button";
+export { default as ButtonBack } from "./ButtonBack";
 export { default as ThemedGradient } from "./ThemedGradient";
