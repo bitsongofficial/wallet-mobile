9582009cebf8cda59bf93840332804457305af31
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-03 18:35:35 +0300

    feat: ✨ added numpad

--- screens/SendModalScreens/components/index.ts
+++ screens/SendModalScreens/components/index.ts
@@ -1,3 +1,4 @@
 export { default as CardAdress } from "./CardAdress";
 export { default as CardAdressSelf } from "./CardAdressSelf";
 export { default as CardSelectCoin } from "./CardSelectCoin";
+export { default as Numpad } from "./Numpad";
