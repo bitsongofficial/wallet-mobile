27faaf94361f9d3c3d18b47bda636686a9c4ac78
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-03 18:32:53 +0300

    feat: ✨ added self address view

---
+++ screens/SendModalScreens/components/CardAdressSelf.tsx
@@ -0,0 +1,42 @@
+import { StyleProp, StyleSheet, Text, View, ViewStyle } from "react-native";
+import { observer } from "mobx-react-lite";
+import { Card, Icon } from "components/atoms";
+import { useTheme } from "hooks";
+import { Coin } from "classes";
+
+type Props = {
+  coin?: Coin;
+  style?: StyleProp<ViewStyle>;
+};
+
+export default observer<Props>(function CardWallet({ coin, style }) {
+  const theme = useTheme();
+  if (!coin) return null;
+  const { address } = coin.info;
+  const shortAd = `${address.substring(0, 16)}..${address.slice(-7)}`;
+
+  return (
+    <Card style={[styles.card, style]}>
+      <Icon name="arrow_up" />
+      <View style={styles.description}>
+        <Text style={[theme.text.primary]}>{shortAd}</Text>
+        <Text style={[theme.text.secondary]}>Sent on Feb 15</Text>
+      </View>
+    </Card>
+  );
+});
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#14142E",
+    height: 70,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 29,
+    paddingVertical: 18,
+    // justifyContent: "center",
+  },
+  description: {
+    marginLeft: 16,
+  },
+});
--- screens/SendModalScreens/components/index.ts
+++ screens/SendModalScreens/components/index.ts
@@ -1,1 +1,2 @@
 export { default as CardAdress } from "./CardAdress";
+export { default as CardAdressSelf } from "./CardAdressSelf";
