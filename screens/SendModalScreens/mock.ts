561b5ec1b7590e443d1e8303b325eaf755e0b7c0
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-03 18:36:29 +0300

    feat: ✨ added user's view

---
+++ screens/SendModalScreens/mock.ts
@@ -0,0 +1,28 @@
+import { IUser } from "./components";
+
+export const users: IUser[] = [
+  {
+    _id: "1",
+    avatar: "",
+    firstName: "A",
+    lastName: "Delogu",
+  },
+  {
+    _id: "2",
+    avatar: "",
+    firstName: "M",
+    lastName: "Vacchi",
+  },
+  {
+    _id: "3",
+    avatar: "",
+    firstName: "L",
+    lastName: "Aleandri",
+  },
+  {
+    _id: "4",
+    avatar: "",
+    firstName: "A",
+    lastName: "Rossi",
+  },
+];
