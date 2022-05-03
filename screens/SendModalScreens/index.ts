59d8bc5749f7fcc3b5af006366702caf7f8de2f6
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-03 18:48:37 +0300

    feat: ✨ added send recap screen

--- screens/SendModalScreens/index.ts
+++ screens/SendModalScreens/index.ts
@@ -1,3 +1,4 @@
 export { default as InsertImport } from "./InsertImport";
 export { default as SelectCoin } from "./SelectCoin";
 export { default as SelectReceiver } from "./SelectReceiver";
+export { default as SendRecap } from "./SendRecap";
