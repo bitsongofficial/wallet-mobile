e45b8217cf4b0b8b69fc55ec5a0d61ad3aba3937
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-06 18:14:30 +0300

    feat(atom): ✨  added Badge

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as Button } from "./Button";
 export { default as ButtonBack } from "./ButtonBack";
 export { default as ThemedGradient } from "./ThemedGradient";
 export { default as Card } from "./Card";
+export { default as Badge } from "./Badge";
