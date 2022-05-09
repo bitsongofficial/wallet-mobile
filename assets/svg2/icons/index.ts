86c457d9cdca54b0a0f944825ab2d4d3d3f001af
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-09 18:33:06 +0300

    chore: added new icons

---
+++ assets/svg2/icons/index.ts
@@ -0,0 +1,9 @@
+import chevron_right from "./chevron-right.svg";
+import eye_closed_gradient from "./eye-closed_gradient.svg";
+import eye from "./eye.svg";
+
+export default <const>{
+  chevron_right,
+  eye_closed_gradient,
+  eye,
+};
