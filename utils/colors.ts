8c51061b479d68189340c58a249a7d31f7b91df0
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-09 19:30:47 +0300

    feat(utils): added colors, util

---
+++ utils/colors.ts
@@ -0,0 +1,13 @@
+enum COLOR {
+  White = '#FFFFFF',
+  /** Text Logo */
+  Grey1 = '#6B6B84',
+
+  Dark1 = '#373764',
+  /** Modal */
+  Dark2 = '#2B2B43',
+  /** Text Black */
+  Dark3 = '#14142E',
+}
+
+export default COLOR;
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+export { default as COLOR } from "./colors";
+
 export function hexAlpha(hex: string, percent: number): string {
   const str = Math.trunc((percent * 255) / 100).toString(16);
   return hex + (str.length === 2 ? str : `0${str}`);
@@ -5,4 +7,12 @@ export function hexAlpha(hex: string, percent: number): string {
 
 export function round(v: number, num = 2) {
   return +v.toFixed(num);
+}
+
+export function sliceIntoChunks<T>(arr: T[], size: number) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size));
+  }
+  return res;
 }
