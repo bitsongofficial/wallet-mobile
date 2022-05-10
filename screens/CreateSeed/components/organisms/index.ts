0d1716e27e2dc6b2e30bcd64f6ecf25d23d24e03
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-10 18:44:50 +0300

    feat: 🦠 added WalletName

--- screens/CreateSeed/components/organisms/index.ts
+++ screens/CreateSeed/components/organisms/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
 export { default as CreateSeed } from "./CreateSeed";
+export { default as WalletName } from "./WalletName";
 export { default as SetPin } from "./SetPin";
