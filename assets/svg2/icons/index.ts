84acbc3c2704edeeb382db7614e95049145ca70c
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-10 18:30:29 +0300

    feat(icon): added logo.svg

--- assets/svg2/icons/index.ts
+++ assets/svg2/icons/index.ts
@@ -1,9 +1,11 @@
 import chevron_right from "./chevron-right.svg";
 import eye_closed_gradient from "./eye-closed_gradient.svg";
 import eye from "./eye.svg";
+import logo from "./logo.svg";
 
 export default <const>{
   chevron_right,
   eye_closed_gradient,
   eye,
+  logo,
 };
