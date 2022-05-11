5edc0f8624a8ad47d280a0f5dfc83a21899eb934
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-11 19:16:45 +0300

    feat: 🦠  added Footer

---
+++ screens/CreateSeed/components/organisms/Footer.tsx
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Button, ButtonBack, Icon2 } from "components/atoms";
+import { useTheme } from "hooks";
+
+type Props = {
+  onPressBack(): void;
+  onPressNext(): void;
+  nextButtonText: string;
+};
+
+export default ({ onPressBack, onPressNext, nextButtonText }: Props) => {
+  const theme = useTheme();
+  return (
+    <View style={styles.container}>
+      <View style={styles.left}>
+        <ButtonBack onPress={onPressBack} />
+      </View>
+
+      <View style={styles.right}>
+        <Button
+          contentContainerStyle={styles.buttonContent}
+          onPress={onPressNext}
+          // onPress={open}
+          // disable={!biometric.access}
+          // IconRight={<Icon name="arrow_r" size={10} />}
+        >
+          <Text style={[styles.buttonText, theme.text.primary]}>Continue</Text>
+          <Icon2 name="chevron_right" size={18} />
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  left: { flex: 2, marginRight: 16 },
+  right: { flex: 2 },
+
+  buttonContent: {
+    paddingVertical: 18,
+    paddingHorizontal: 24,
+    justifyContent: "space-between",
+  },
+  buttonText: {
+    fontFamily: "CircularStd",
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: 16,
+    lineHeight: 20,
+  },
+});
--- screens/CreateSeed/components/organisms/index.ts
+++ screens/CreateSeed/components/organisms/index.ts
@@ -1,4 +1,4 @@
 export { default as Header } from "./Header";
 export { default as CreateSeed } from "./CreateSeed";
-export { default as WalletName } from "./WalletName";
 export { default as SetPin } from "./SetPin";
+export { default as Footer } from "./Footer";
