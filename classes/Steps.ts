4430b24798d6a7673d8954a33faf05b07d1c7205
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-11 19:12:05 +0300

    feat(class): add Steps mobx

---
+++ classes/Steps.ts
@@ -0,0 +1,29 @@
+import { makeAutoObservable } from "mobx";
+
+export default class Steps<T extends string> {
+  active = 0;
+
+  constructor(public titles: T[]) {
+    makeAutoObservable(this, {}, { autoBind: true });
+  }
+
+  get title() {
+    return this.titles[this.active];
+  }
+
+  private setActive(index: number) {
+    this.active = index;
+  }
+
+  next() {
+    if (this.active < this.titles.length - 1) {
+      this.setActive(this.active + 1);
+    }
+  }
+
+  prev() {
+    if (this.active > 0) {
+      this.setActive(this.active - 1);
+    }
+  }
+}
