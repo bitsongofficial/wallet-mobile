3a14cf1c622f02ed25c78fe2fbb40b07a086000d
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-12 19:04:44 +0300

    feat: added generate seed to Phrase

--- classes/Phrase.ts
+++ classes/Phrase.ts
@@ -1,4 +1,6 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
+import { Buffer } from "buffer";
+import { bip39, getRandomValues, InputHandler } from "utils";
 
 export default class Phrase {
   words: string[] = [];
@@ -11,17 +13,62 @@ export default class Phrase {
     return true; // TODO: calc me
   }
 
-  create() {
-    this.words = Phrase.mock;
+  async create() {
+    const words = await Phrase.generate();
+    runInAction(() => {
+      this.words = words;
+    });
   }
 
-  add(word: string) {
-    this.words?.push(word);
+  addWord(word: string) {
+    this.words?.push(word.toLowerCase());
   }
 
-  set(words: string[]) {
+  setWords(words: string[]) {
     this.words = words;
   }
 
+  // -------------
+
+  private input = new InputHandler();
+
+  get inputValue() {
+    return this.input.value;
+  }
+
+  get hint() {
+    const value = this.input.value.toLowerCase();
+
+    if (!value) return null;
+
+    return Phrase.wordlist.find((word: string) =>
+      word.toLowerCase().startsWith(value)
+    );
+  }
+
+  inputSubmit() {
+    if (this.hint) {
+      this.addWord(this.hint);
+      this.input.clear();
+    }
+  }
+
+  inputSet(value?: string) {
+    this.input.set(value?.toLowerCase());
+  }
+
+  // -----------------
+
   static mock = new Array<string>(24).fill("test");
+
+  static wordlist: string[] = bip39.wordlists.EN;
+
+  static async generate(strength: number = 256) {
+    if (strength % 32 !== 0) {
+      throw new TypeError("invalid entropy");
+    }
+
+    const buffer = Buffer.from(getRandomValues(new Uint8Array(strength / 8)));
+    return bip39.entropyToMnemonic(buffer.toString("hex")).split(" ");
+  }
 }
