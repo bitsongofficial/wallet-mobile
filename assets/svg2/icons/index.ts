9185960c7382610a1b3e279b2eff04ad893d9323
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-13 18:15:55 +0300

    feat(assets): 🏞 added .svg

--- assets/svg2/icons/index.ts
+++ assets/svg2/icons/index.ts
@@ -1,13 +1,41 @@
 import chevron_right from "./chevron-right.svg";
 import chevron_left from "./chevron-left.svg";
 import eye_closed_gradient from "./eye-closed_gradient.svg";
+import fingerprint_gradient from "./fingerprint_gradient.svg";
 import eye from "./eye.svg";
 import logo from "./logo.svg";
+import stake from "./stake.svg";
+import like from "./like.svg";
+import home from "./home.svg";
+import circle from "./circle.svg";
+import arrow_down from "./arrow-down.svg";
+import arrow_up from "./arrow-up.svg";
+import inquire from "./inquire.svg";
+import scan from "./scan.svg";
+import settings from "./settings.svg";
+import claim from "./claim.svg";
+import restake from "./restake.svg";
+import unstake from "./unstake.svg";
+import upNdown from "./upNdown.svg";
 
 export default <const>{
-  chevron_right,
   eye_closed_gradient,
+  fingerprint_gradient,
+  chevron_right,
   chevron_left,
   eye,
   logo,
+  stake,
+  like,
+  home,
+  circle,
+  arrow_down,
+  arrow_up,
+  inquire,
+  scan,
+  settings,
+  claim,
+  restake,
+  unstake,
+  upNdown,
 };
