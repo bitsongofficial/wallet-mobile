74a74bfbb5511427c4ff1334441a4f6466724bb2
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-17 18:54:09 +0300

    feat(classes): added transaction creater

---
+++ classes/Transaction/Creater.ts
@@ -0,0 +1,44 @@
+import Coin from "classes/Coin";
+import { IPerson, ITransaction } from "classes/types";
+import { makeAutoObservable } from "mobx";
+// import { ITransaction } from "";
+
+export default class TransactionCreater {
+  coin: Coin | null = null;
+  amount: string = ""; // usd
+  address: string | null = null;
+  receiver?: IPerson | null = null; //
+
+  constructor() {
+    makeAutoObservable(this, {}, { autoBind: true });
+  }
+
+  setCoin(coin: Coin) {
+    this.coin = coin;
+  }
+  setAmount(amount: string) {
+    this.amount = amount;
+  }
+  setAddress(address: string) {
+    this.address = address;
+  }
+  setReceiver(receiver: IPerson) {
+    this.receiver = receiver;
+  }
+
+  get isAddressValid() {
+    // TODO: upd. this
+    return !!this.address;
+  }
+
+  get isReady() {
+    return !!this.coin && !!this.amount && this.isAddressValid && this.receiver;
+  }
+
+  create(): ITransaction {
+    if (this.isReady) {
+      const { address, amount, coin, receiver } = this;
+      return { address, amount, coin, receiver };
+    }
+  }
+}
---
+++ classes/Transaction/Transaction.ts
@@ -0,0 +1,6 @@
+import Creater from "./Creater";
+// import { ITransaction } from "";
+
+export default class Transaction {
+  static Creater = Creater;
+}
