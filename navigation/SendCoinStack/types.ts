e155584cfc0a8d62f02c431e50b1ae45d6614a76
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-20 18:22:14 +0300

    refactor: remove unused SendCoinStack

--- navigation/SendCoinStack/types.ts
+++
@@ -1,7 +0,0 @@
-export type SendCoinStackParamList = {
-  SelectReceiver: undefined;
-  InsertImport: undefined;
-  SelectCoin: undefined;
-  SendRecap: undefined;
-  ScannerQR: { onBarCodeScanned(data: string): void };
-};
