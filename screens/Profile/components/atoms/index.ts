aad5c0f1154b109cbd87d4dd79bab3bdda833ef5
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-24 18:41:08 +0300

    feat: ⚛ added Profile Agrements

--- screens/Profile/components/atoms/index.ts
+++ screens/Profile/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Avatar } from "./Avatar";
 export { default as Title } from "./Title";
 export { default as Subtitle } from "./Subtitle";
 export { default as ListButton } from "./ListButton";
+export { default as Agreement } from "./Agreement";
