86a12ecb86a5eedb27e1189626adbd0535028d75
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-24 18:42:33 +0300

    feat: upd. ListButton

--- screens/Profile/components/atoms/ListButton.tsx
+++ screens/Profile/components/atoms/ListButton.tsx
@@ -4,6 +4,7 @@ import { Button, Icon2, IconName } from "components/atoms";
 import { useTheme } from "hooks";
 import { COLOR, hexAlpha } from "utils";
 import { Avatar } from "../atoms";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 type Props = {
   style?: StyleProp<ViewStyle>;
@@ -12,22 +13,56 @@ type Props = {
   icon: IconName;
   children?: string;
   text?: string;
+  Right?: JSX.Element;
 };
 
-export default observer<Props>(({ style, icon, children, text }) => {
-  const theme = useTheme();
-  return (
-    <View style={[styles.container, style]}>
-      <Icon2 name={icon} size={24} style={styles.icon} />
-      <Text style={[styles.text, theme.text.primary]}>{children || text}</Text>
-    </View>
-  );
-});
+export default observer<Props>(
+  ({ style, icon, children, text, arrow, Right, onPress }) => {
+    const theme = useTheme();
+    return (
+      <TouchableOpacity onPress={onPress} style={style}>
+        <View style={styles.container}>
+          <View style={styles.left}>
+            <Icon2
+              name={icon}
+              size={24}
+              style={styles.icon}
+              stroke={hexAlpha(COLOR.White, 50)}
+            />
+            <Text style={[styles.text, theme.text.primary]}>
+              {children || text}
+            </Text>
+          </View>
+          <View style={styles.right}>
+            {Right}
+            {arrow && (
+              <Icon2 name="chevron_right" size={16} stroke={COLOR.White} />
+            )}
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
+    paddingVertical: 10,
+    paddingLeft: 15,
+  },
+
+  left: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  right: {
+    flex: 1,
+    flexDirection: "row",
+
+    justifyContent: "flex-end",
   },
 
   icon: {
