5de3a07e492f4079e37da7339ace1dd3b7bcb5d8
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-24 18:44:16 +0300

    feat: added User and mock DApp in MainStore

--- stores/MainStore.ts
+++ stores/MainStore.ts
@@ -1,3 +1,4 @@
+import { DApp, User } from "classes";
 import { makeAutoObservable } from "mobx";
 import SettingsStore from "./SettingsStore";
 import WalletStore from "./WalletStore";
@@ -6,6 +7,9 @@ export default class MainStore {
   auth = null;
   wallet = new WalletStore();
   settings = new SettingsStore();
+
+  user: null | User = new User(); //  null;
+  dapp = new DApp(); // MOck DApp functions
 
   constructor() {
     makeAutoObservable(this, {}, { autoBind: true });
