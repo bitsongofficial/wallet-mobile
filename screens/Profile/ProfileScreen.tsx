39109c2db6e0dc57b932ae6d9020341123cd4c8a
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-24 18:45:13 +0300

    feat: upd. ProfileScreen

--- screens/Profile/ProfileScreen.tsx
+++ screens/Profile/ProfileScreen.tsx
@@ -5,34 +5,192 @@ import { observer } from "mobx-react-lite";
 import { RootStackParamList } from "types";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useStore } from "hooks";
-import { ThemedGradient } from "components/atoms";
-import { ListButton, Subtitle } from "./components/atoms";
+import { Button, ThemedGradient } from "components/atoms";
+import { Agreement, ListButton, Subtitle, Title } from "./components/atoms";
 import { Head } from "./components/moleculs";
+import { useSpring } from "@react-spring/native";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { reaction } from "mobx";
+import { ScrollView } from "react-native-gesture-handler";
+import { COLOR, InputHandler } from "utils";
 
 type ValueTabs = "Coins" | "Fan Tokens";
-
-// const tabs: ValueTabs[] = ["Coins", "Fan Tokens"];
 
 type Props = NativeStackScreenProps<RootStackParamList, "Profile">;
 
 export default observer<Props>(function MainScreen({ navigation }) {
-  const {} = useStore();
+  const { settings, user, dapp } = useStore();
+
+  const [isOpenInput, setOpenInput] = useState(false);
+  const openInput = useCallback(() => setOpenInput(true), []);
+  const inputNick = useMemo(() => new InputHandler(), []);
+
+  const [isNickValid, setIsNickValid] = useState(false);
+  // TODO: need debouncer
+  const checkNick = async (nick: string) =>
+    setIsNickValid(await dapp.checkNick(nick));
+
+  useEffect(() => reaction(() => inputNick.value, checkNick), []);
+
+  const navToPrivacy = useCallback(() => {}, []);
+  const navToTerms = useCallback(() => {}, []);
+  const disconnectAndRemove = useCallback(() => {}, []);
+
+  const openAddNewaccount = useCallback(() => {}, []);
+  const openAddWatchaccount = useCallback(() => {}, []);
+  const openSecurity = useCallback(() => {}, []);
+  const openAddressBook = useCallback(() => {}, []);
+  const openNotifications = useCallback(() => {}, []);
+  const openWalletConnect = useCallback(() => {}, []);
+  const openLanguages = useCallback(() => {}, []);
+  const openCurrency = useCallback(() => {}, []);
+  const toggleNightMode = useCallback(() => {}, []);
+  const openCurrencyApp = useCallback(() => {}, []);
+  const openFAQ = useCallback(() => {}, []);
+  const openTermsAndConditions = useCallback(() => {}, []);
+  const openPrivacyPolicy = useCallback(() => {}, []);
+
   return (
     <>
       <StatusBar style="light" />
 
       <ThemedGradient style={styles.container}>
         <SafeAreaView style={styles.container}>
-          <Head style={styles.head} />
+          <ScrollView>
+            <Head
+              style={styles.head}
+              input={inputNick}
+              isOpenInput={isOpenInput}
+              onPressSetNick={openInput}
+            />
+            <View style={[styles.wrapper]}>
+              <Subtitle style={styles.subtitle}>Connected with</Subtitle>
+              {/* <Wallet /> */}
+              <ListButton
+                onPress={openAddNewaccount}
+                icon="wallet"
+                arrow
+                style={styles.listButton}
+              >
+                Add a new account
+              </ListButton>
+              {/* todo change eye.svg */}
+              <ListButton onPress={openAddWatchaccount} icon="eye" arrow>
+                Add a Watch account
+              </ListButton>
+              <Agreement
+                onPressPrivacy={navToPrivacy}
+                onPressTerms={navToTerms}
+                style={styles.agreement}
+              />
 
-          <Subtitle>Connected with</Subtitle>
-          {/* <Wallet /> */}
-          <ListButton>Add a new account</ListButton>
-          <ListButton>Add a Watch account</ListButton>
-          <Text>
-            By creating an account, you’re agree to Cosmonautico {"\n"}
-            Terms and Conditions & Privacy Policy
-          </Text>
+              <Title style={styles.title}>Settings</Title>
+
+              <View style={styles.section}>
+                <Subtitle style={styles.subtitle}>Account</Subtitle>
+                <ListButton
+                  onPress={openSecurity}
+                  icon="star_shield"
+                  arrow
+                  style={styles.listButton}
+                >
+                  Security
+                </ListButton>
+                <ListButton
+                  onPress={openAddressBook}
+                  icon="address_book"
+                  arrow
+                  style={styles.listButton}
+                >
+                  Address Book
+                </ListButton>
+                <ListButton
+                  onPress={openNotifications}
+                  icon="bell"
+                  style={styles.listButton}
+                >
+                  Notifications
+                </ListButton>
+                <ListButton
+                  onPress={openWalletConnect}
+                  arrow
+                  style={styles.listButton}
+                >
+                  Wallet Connect
+                </ListButton>
+              </View>
+
+              <View style={styles.section}>
+                <Subtitle style={styles.subtitle}>App Preferences</Subtitle>
+                <ListButton
+                  onPress={openLanguages}
+                  icon="translate"
+                  style={styles.listButton}
+                >
+                  Language
+                </ListButton>
+                <ListButton
+                  onPress={openCurrency}
+                  icon="circle_dollar"
+                  style={styles.listButton}
+                >
+                  Currency
+                </ListButton>
+                <ListButton
+                  onPress={toggleNightMode}
+                  icon="moon"
+                  style={styles.listButton}
+                >
+                  Night Mode
+                </ListButton>
+              </View>
+
+              <View style={styles.section}>
+                <Subtitle style={styles.subtitle}>Support</Subtitle>
+                <ListButton
+                  onPress={openCurrencyApp}
+                  icon="star"
+                  arrow
+                  style={styles.listButton}
+                >
+                  Currency App
+                </ListButton>
+                <ListButton
+                  onPress={openFAQ}
+                  icon="chat_dots"
+                  arrow
+                  style={styles.listButton}
+                >
+                  FAQ
+                </ListButton>
+                <ListButton
+                  onPress={openTermsAndConditions}
+                  icon="file_text"
+                  arrow
+                  style={styles.listButton}
+                >
+                  Terms and conditions
+                </ListButton>
+                <ListButton
+                  onPress={openPrivacyPolicy}
+                  icon="file_text"
+                  arrow
+                  style={styles.listButton}
+                >
+                  Privacy Policy
+                </ListButton>
+              </View>
+
+              <Button
+                mode="fill"
+                text="Disconnect and Remove Wallet"
+                onPress={disconnectAndRemove}
+                style={styles.button}
+                textStyle={styles.buttonText}
+                contentContainerStyle={styles.buttonContent}
+              />
+            </View>
+          </ScrollView>
         </SafeAreaView>
       </ThemedGradient>
     </>
@@ -40,18 +198,44 @@ export default observer<Props>(function MainScreen({ navigation }) {
 });
 
 const Wallet = () => {};
+const Switch = () => {
+  const isActive = useSpring({
+    width: active ? 19 : 8,
+    backgroundColor: active ? COLOR.White : "#5A5A6D",
+  });
+  const {} = useSpring();
+  return (
+    <View
+      style={{
+        width: 55,
+        height: 28,
+        backgroundColor: COLOR.Dark3,
+        borderRadius: 20,
+      }}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  // ----------------
-
+  container: { flex: 1 },
   head: {
     marginHorizontal: 25, // <- wrapper
     marginBottom: 30,
   },
 
-  // ----------------
+  wrapper: { marginHorizontal: 34 },
+  wrapper_opecity: { opacity: 0.1 },
+  agreement: { marginBottom: 54, marginTop: 25 },
+  title: { marginBottom: 38 },
+  section: { marginBottom: 35 },
+  subtitle: { marginBottom: 22 },
+
+  listButton: { marginTop: 4 },
+
+  button: { backgroundColor: COLOR.Dark3 },
+  buttonContent: { paddingVertical: 18 },
+  buttonText: {
+    fontSize: 14,
+    lineHeight: 18,
+  },
 });
