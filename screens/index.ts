42db413ae8b12a69f1e5c3775504c0ffb4ed8236
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-25 19:03:42 +0300

    feat(screen): ✨ added SettingNotifications

--- screens/index.ts
+++ screens/index.ts
@@ -6,4 +6,5 @@ export { default as TabTwoScreen } from "./TabTwoScreen";
 export { default as Main } from "./MainScreen";
 export { default as Stacking } from "./StackingScreen";
 export { default as Profile } from "./Profile";
+export { default as SettingsNotifications } from "./Profile/SettingsNotificationsScreen";
 export { CreateWallet, ImportFromSeed, ImportWithKeplr } from "./CreateSeed";
