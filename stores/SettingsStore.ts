86f3105b0a13a1c5263d6ef6dc83ae9eafa255e9
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-27 19:50:04 +0300

    feat: make observable SettingsStore

--- stores/SettingsStore.ts
+++ stores/SettingsStore.ts
@@ -1,9 +1,18 @@
+import { makeAutoObservable } from "mobx";
+
 export default class SettingsStore {
   theme: "light" | "dark" = "dark";
+  language: string = "en";
 
-  constructor() {}
+  constructor() {
+    makeAutoObservable(this, {}, { autoBind: true });
+  }
 
   setTheme(theme: "light" | "dark") {
     this.theme = theme;
+  }
+
+  setLenguage(language: string) {
+    this.language = language;
   }
 }
