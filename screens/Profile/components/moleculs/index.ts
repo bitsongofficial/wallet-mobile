0c2e0982d059d7870fa8a1688591a0654ad98b14
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-30 19:37:46 +0300

    feat: 🧬 + WalletItem

--- screens/Profile/components/moleculs/index.ts
+++ screens/Profile/components/moleculs/index.ts
@@ -3,4 +3,5 @@ export { default as ChangeAvatar } from "./ChangeAvatar";
 export { default as AddAccount } from "./AddAccount";
 export { default as LanguageItem } from "./LanguageItem";
 export { default as CurrencyItem } from "./CurrencyItem";
+export { default as WalletItem } from "./WalletItem";
 export { default as GenerateMnenonic } from "./GenerateMnenonic";
