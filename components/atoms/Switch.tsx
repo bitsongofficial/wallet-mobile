ac5356a739799180453b28da434904ad1aff6064
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-05-30 19:33:50 +0300

    feat: ⚛ upd. Switch, added gradient

--- components/atoms/Switch.tsx
+++ components/atoms/Switch.tsx
@@ -1,21 +1,20 @@
 import { useCallback, useState } from "react";
 import { StyleSheet, View } from "react-native";
-import {
-  TouchableWithoutFeedback,
-  TouchableNativeFeedback,
-  TapGestureHandler,
-  BaseButton,
-} from "react-native-gesture-handler";
+import { BaseButton } from "react-native-gesture-handler";
 import { animated, useSpring } from "@react-spring/native";
 import { COLOR } from "utils";
+import ThemedGradient from "./ThemedGradient";
 
 type SwitchProps = {
   active?: boolean;
   onPress?(): void;
+  gradient?: boolean;
 };
 
-export default ({ onPress, active = false }: SwitchProps) => {
+export default ({ onPress, active = false, gradient }: SwitchProps) => {
   const activeStyle = useSpring({ left: active ? 31 : 4 });
+
+  const Background = gradient ? ThemedGradient : View;
 
   // console.log("isActive", isActive);
   // isActive;
@@ -23,10 +22,12 @@ export default ({ onPress, active = false }: SwitchProps) => {
   // TODO:Check on phisic IOS
   return (
     <BaseButton onPress={onPress}>
-      <View style={styles.container}>
-        <animated.View style={[styles.dot, activeStyle]}>
-          <></>
-        </animated.View>
+      <View style={[styles.container, !gradient && styles.background]}>
+        <Background style={{ flex: 1 }}>
+          <animated.View style={[styles.dot, activeStyle]}>
+            <></>
+          </animated.View>
+        </Background>
       </View>
     </BaseButton>
   );
@@ -36,11 +37,13 @@ const styles = StyleSheet.create({
   container: {
     width: 55,
     height: 28,
-    backgroundColor: COLOR.Dark3,
     borderRadius: 20,
+    overflow: "hidden",
+  },
+  background: {
+    backgroundColor: COLOR.Dark3,
   },
   dot: {
-    //
     borderRadius: 30,
     width: 20,
     height: 20,
