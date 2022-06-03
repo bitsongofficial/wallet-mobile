5026dc9f4bbc8afea916dfa4815c9b4526393377
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-06-03 19:50:06 +0300

    feat(constants): + currencies, languages mock

--- screens/Profile/type.ts
+++ screens/Profile/type.ts
@@ -1,5 +1,17 @@
-export type Wallet = {
+export type IWallet = {
   address: string;
   name: string;
   type: "one" | "two";
+};
+
+export type ILang = {
+  name: string;
+  value: string;
+  id: string;
+};
+
+export type ICurrency = {
+  _id: string;
+  name: string;
+  title: string;
 };
