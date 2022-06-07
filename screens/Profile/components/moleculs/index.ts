104852e59f3ad13f4efb5ec94114476d02f4db06
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-06-07 18:52:23 +0300

    feat(profile):🦠 upd. GenerateMnemonic bottomsheet

--- screens/Profile/components/moleculs/index.ts
+++ screens/Profile/components/moleculs/index.ts
@@ -4,4 +4,3 @@ export { default as LanguageItem } from "./LanguageItem";
 export { default as CurrencyItem } from "./CurrencyItem";
 export { default as WalletItemEdited } from "./WalletItemEdited";
 export { default as WalletItem } from "./WalletItem";
-export { default as GenerateMnenonic } from "./GenerateMnenonic";
--- screens/Profile/components/organisms/index.ts
+++ screens/Profile/components/organisms/index.ts
@@ -1,4 +1,5 @@
 export { default as ChangeLanguage } from "./ChangeLanguage";
 export { default as ChangeCurrency } from "./ChangeCurrency";
 export { default as ChangeWallet } from "./ChangeWallet";
+export { default as GenerateMnenonic } from "./GenerateMnenonic";
 export { default as AddAccount } from "./AddAccount";
