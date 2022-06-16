32f8eca6399ae302fe8235b2c2d742d8b37a3df3
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-06-16 14:43:50 +0300

    feat: 🦠 + FaceID

--- screens/CreateSeed/components/organisms/index.ts
+++ screens/CreateSeed/components/organisms/index.ts
@@ -3,3 +3,4 @@ export { default as CreateSeed } from "./CreateSeed";
 export { default as SetPin } from "./SetPin";
 export { default as PhraseInput } from "./PhraseInput";
 export { default as Footer } from "./Footer";
+export { default as FaceID } from "./FaceID";
