9df4815776bad29972f1d20dec38acb67dba26c1
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-06-21 17:31:45 +0300

    refactor: ⚛ added InputWord, PhraseHorisontal

--- components/moleculs/index.ts
+++ components/moleculs/index.ts
@@ -1,4 +1,6 @@
 export { BottomSheetModal, BottomSheet } from "./BottomSheetModal";
 export { default as Phrase } from "./Phrase";
+export { default as PhraseHorisontal } from "./PhraseHorisontal";
 export { default as Pagination } from "./Pagination";
 export { default as Numpad } from "./Numpad";
+export { default as InputWord } from "./InputWord";
