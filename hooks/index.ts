cb31a7aefc9b6231c553c79f3e4bade814adf3e7
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-06-27 15:56:01 +0300

    chore: fix import

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,4 +2,5 @@ export { default as useCachedResources } from "./useCachedResources";
 export { default as useColorScheme } from "./useColorScheme";
 export { default as useStore } from "./useStore";
 export { default as useTheme } from "./useTheme";
+export { default as useLoading } from "./useLoading";
 export { default as useSeedPhrase } from "./useSeedPhrase";
