a93c1e4e4db92dd93d5b079ab2f2ef0d52ec5e14
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-06-27 12:31:18 +0300

    refactor: fix setBackgroundColorAsync

--- App.tsx
+++ App.tsx
@@ -1,33 +1,35 @@
-import "./shim"
+import "./shim";
 
-import { StatusBar } from "expo-status-bar"
-import { Alert, StyleSheet, Text, View } from "react-native"
-import { SafeAreaProvider } from "react-native-safe-area-context"
-import { GestureHandlerRootView } from "react-native-gesture-handler"
-import { configure } from "mobx"
-import useCachedResources from "./hooks/useCachedResources"
-import useColorScheme from "./hooks/useColorScheme"
-import Navigation from "./navigation"
-import { test } from "core/Test"
-import { useEffect } from "react"
+import { StatusBar } from "expo-status-bar";
+import { Alert, Platform, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { configure } from "mobx";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import { test } from "core/Test";
+import { useEffect } from "react";
 import { COLOR } from "utils";
 import * as NavigationBar from "expo-navigation-bar";
-import FullscreenOverlay from "components/atoms/FullscreenOverlay"
-import { Loader } from "components/atoms"
-import { useStore } from "hooks"
-import { setUpPushNotificationsEvents } from "utils/pushNotifications"
-import { observer } from "mobx-react-lite"
+import FullscreenOverlay from "components/atoms/FullscreenOverlay";
+import { Loader } from "components/atoms";
+import { useStore } from "hooks";
+import { setUpPushNotificationsEvents } from "utils/pushNotifications";
+import { observer } from "mobx-react-lite";
 
 configure({ useProxies: "ifavailable" });
 
 const App = observer(() => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  const {settings} = useStore()
+  const { settings } = useStore();
 
   useEffect(() => {
-    NavigationBar.setBackgroundColorAsync(COLOR.Dark3);
-    setUpPushNotificationsEvents()
+    if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync(COLOR.Dark3);
+    }
+    setUpPushNotificationsEvents();
   }, []);
 
   if (!isLoadingComplete) {
@@ -39,7 +41,7 @@ const App = observer(() => {
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
           <FullscreenOverlay showing={settings.showLoadingOverlay}>
-            <View style={{display: "flex", justifyContent: "center", alignItems: "center", flex: 1}}>
+            <View style={styles.loaderContainer}>
               <Loader size={60}></Loader>
             </View>
           </FullscreenOverlay>
@@ -47,10 +49,16 @@ const App = observer(() => {
       </GestureHandlerRootView>
     );
   }
-})
+});
 
 const styles = StyleSheet.create({
   gestureHandler: { flex: 1 },
+  loaderContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
 });
 
-export default App
+export default App;
