41c053bbd1430c93f1b88bfdd0121688402e959f
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-06-29 18:52:11 +0300

    feat: upd. AdressBook screen #25

--- screens/Profile/AddressBookScreen.tsx
+++ screens/Profile/AddressBookScreen.tsx
@@ -1,51 +1,72 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useState } from "react";
 import {
   ListRenderItem,
+  SectionList,
   StyleProp,
   StyleSheet,
+  Text,
   View,
   ViewStyle,
 } from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
+  FlatList,
   RectButton,
   Swipeable,
   TouchableOpacity,
 } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { observer } from "mobx-react-lite";
+import { observable } from "mobx";
 import { RootStackParamList } from "types";
 import { useStore } from "hooks";
+import { COLOR, hexAlpha, InputHandler } from "utils";
 import { Button, Icon2, ThemedGradient } from "components/atoms";
-import { COLOR, InputHandler } from "utils";
 import { Circles, Search, Subtitle, Title } from "./components/atoms";
-import { observable } from "mobx";
-import { WalletItem } from "./components/moleculs";
-import { StoreWallet } from "stores/WalletStore";
+import { ContactItem } from "./components/moleculs";
+import { AddContact } from "./components/organisms";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
+import { IPerson } from "classes/types";
 
 type Props = NativeStackScreenProps<RootStackParamList, "AddressBook">;
 
 export default observer<Props>(function AddressBookScreen({ navigation }) {
-  const { wallet } = useStore();
+  const { contacts } = useStore();
+  // -------- Sections Data ----------
 
   // ------- Wallets ------
-  const wallets = wallet.wallets;
   const mapItemsRef = useMemo(
-    () => observable.map<StoreWallet, React.RefObject<Swipeable>>(),
+    () => observable.map<IPerson, React.RefObject<Swipeable>>(),
     []
   );
 
-  const renderWallet = useCallback<ListRenderItem<StoreWallet>>(
+  const renderContact = useCallback<ListRenderItem<IPerson>>(
     ({ item }) => (
-      <View style={{ marginBottom: 13 }}>
-        <WalletItem
+      <View style={{ marginBottom: 24 }}>
+        <ContactItem
           value={item}
           onPress={() => {}}
-          onPressDelete={wallet.deleteWallet}
-          // onPressEdit={setEdited}
+          onPressStar={contacts.addToFavorites}
+          onPressDelete={contacts.delete}
+          onPressEdit={openEdit}
           mapItemsRef={mapItemsRef}
         />
+      </View>
+    ),
+    []
+  );
+
+  const renderSectionHeader = useCallback(
+    ({ section }) => (
+      <View style={[{ marginBottom: 8 }, styles.wrapper]}>
+        <Text style={{ color: hexAlpha(COLOR.White, 40) }}>
+          {section.label}
+        </Text>
       </View>
     ),
     []
@@ -55,62 +76,108 @@ export default observer<Props>(function AddressBookScreen({ navigation }) {
 
   const input = useMemo(() => new InputHandler(), []);
 
+  // ------- BottomSheet ----------
+
+  const currentPosition = useSharedValue(0);
+  const animStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(currentPosition.value, [0, 350], [0, 0.5]);
+    return {
+      flex: 1,
+      opacity,
+    };
+  });
+
+  const [isShowAddContact, setShowAddContact] = useState(false);
+  const closeAddContact = useCallback(() => setShowAddContact(false), []);
+  const openAddContact = useCallback(() => setShowAddContact(true), []);
+
+  const openEdit = useCallback(() => {}, []);
+
+  console.log("contacts.sectionsData", contacts.sectionsData);
+
   return (
     <>
       <StatusBar style="light" />
 
       <ThemedGradient invert style={styles.container}>
         <SafeAreaView style={styles.safeArea}>
-          <Header
-            onPressBack={goBack}
-            style={[styles.header, styles.wrapper]}
-            title="Address Book"
-            onPressScan={() => {}}
-          />
-          <View style={[styles.wrapper, { flex: 1 }]}>
-            <Search
-              value={input.value}
-              onChangeText={input.set}
-              placeholder="Search Address"
-              bottomsheet={false}
+          <Animated.View style={animStyle}>
+            <Header
+              onPressBack={goBack}
+              style={[styles.header, styles.wrapper]}
+              title="Address Book"
+              onPressPlus={openAddContact}
             />
-            <View style={{ alignItems: "center" }}>
-              <View style={{ marginVertical: 40 }}>
-                <Circles>
-                  <Icon2 name="address_book" size={69} stroke={COLOR.White} />
-                </Circles>
-              </View>
-              <Title style={styles.title}>
-                Non hai ancora aggiunto alcun contatto
-              </Title>
-              <Subtitle style={styles.subtitle}>
-                Access VIP experiences, exclusive previews, finance your own
-                music projects and have your say.
-              </Subtitle>
+            <View style={[styles.wrapper]}>
+              <Search
+                value={input.value}
+                onChangeText={input.set}
+                placeholder="Search Address"
+                bottomsheet={false}
+              />
             </View>
+
+            {contacts.persons.length > 0 ? (
+              <SectionList
+                style={{ marginTop: 10 }}
+                contentContainerStyle={{ paddingTop: 30 }}
+                sections={contacts.sectionsData}
+                renderItem={renderContact}
+                renderSectionHeader={renderSectionHeader}
+              />
+            ) : (
+              <View style={[styles.wrapper, { flex: 1 }]}>
+                <View style={{ alignItems: "center" }}>
+                  <View style={{ marginVertical: 40 }}>
+                    <Circles>
+                      <Icon2
+                        name="address_book"
+                        size={69}
+                        stroke={COLOR.White}
+                      />
+                    </Circles>
+                  </View>
+                  <Title style={styles.title}>
+                    Non hai ancora aggiunto alcun contatto
+                  </Title>
+                  <Subtitle style={styles.subtitle}>
+                    Access VIP experiences, exclusive previews, finance your own
+                    music projects and have your say.
+                  </Subtitle>
+                </View>
+              </View>
+            )}
             <View style={styles.buttonContainer}>
               <Button
+                onPress={openAddContact}
                 textStyle={styles.buttonText}
                 contentContainerStyle={styles.buttonContent}
                 mode="fill"
-                text={"Scan QR Code"}
+                text="Add Contact"
               />
             </View>
-          </View>
+          </Animated.View>
         </SafeAreaView>
       </ThemedGradient>
+
+      <AddContact
+        isOpen={isShowAddContact}
+        backgroundStyle={styles.bottomSheetBackground}
+        animatedPosition={currentPosition}
+        onClose={closeAddContact}
+      />
     </>
   );
 });
 
 type PropsHeader = {
   onPressBack(): void;
-  onPressScan(): void;
+  onPressPlus(): void;
   style?: StyleProp<ViewStyle>;
   title?: string;
 };
 
-const Header = ({ onPressBack, style, title, onPressScan }: PropsHeader) => (
+const Header = ({ onPressBack, style, title, onPressPlus }: PropsHeader) => (
   <View style={[styles.header_container, style]}>
     <View style={styles.header_left}>
       <TouchableOpacity onPress={onPressBack} style={styles.header_backButton}>
@@ -120,7 +187,7 @@ const Header = ({ onPressBack, style, title, onPressScan }: PropsHeader) => (
     </View>
     <View style={styles.header_right}>
       <View style={styles.header_scanButtonContainer}>
-        <RectButton style={styles.header_scanButton} onPress={onPressScan}>
+        <RectButton style={styles.header_scanButton} onPress={onPressPlus}>
           <Icon2 name="plus" size={18} stroke={COLOR.White} />
         </RectButton>
       </View>
@@ -193,5 +260,11 @@ const styles = StyleSheet.create({
     height: 33,
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  // ------- BottomSheet --------
+  bottomSheetBackground: {
+    backgroundColor: COLOR.Dark3,
+    paddingTop: 30,
   },
 });
