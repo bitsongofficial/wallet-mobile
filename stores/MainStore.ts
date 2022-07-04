813095309557c6833e7aca599af18da2154741db
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-07-04 13:23:09 +0300

    feat: added mock user data #49

--- stores/MainStore.ts
+++ stores/MainStore.ts
@@ -1,4 +1,4 @@
-import { DApp, User } from "classes";
+import { User } from "classes";
 import { makeAutoObservable } from "mobx";
 import SettingsStore from "./SettingsStore";
 import WalletStore from "./WalletStore";
@@ -9,14 +9,17 @@ import DappConnectionStore from "./DappConnectionStore";
 export default class MainStore {
   auth = null;
   configs = {
-    remote: new RemoteConfigsStore()
+    remote: new RemoteConfigsStore(),
   };
   wallet = new WalletStore(this.configs.remote);
   settings = new SettingsStore();
   coin = new CoinStore(this.wallet, this.configs.remote);
   dapp = new DappConnectionStore(this.wallet, this.configs.remote);
 
-  user: null | User = new User(); //  null;
+  user: null | User = new User({
+    _id: "1234",
+    address: "bitsong16h2ry9axyvzwkftv93h6nusdqeqdn552skxxtw",
+  }); //  null;
 
   constructor() {
     makeAutoObservable(this, {}, { autoBind: true });
