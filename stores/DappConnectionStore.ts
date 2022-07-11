e7f8a9c672c8cf1ae14923f42726099219d081a6
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-07-11 22:27:40 +0200

    feat: :sparkles: added save logic for open walletconnect connections

--- stores/DappConnectionStore.ts
+++ stores/DappConnectionStore.ts
@@ -1,37 +1,108 @@
-import { Transaction } from "classes";
+import AsyncStorageLib from "@react-native-async-storage/async-storage";
+import { Transaction, Wallet } from "classes";
 import { WalletConnectCosmosClientV1 } from "core/connection/WalletConnectV1";
 import { fromAmountToDollars } from "core/utils/Coin";
-import { makeAutoObservable } from "mobx";
+import { autorun, makeAutoObservable, reaction } from "mobx";
 import { openSendRecap } from "screens/SendModalScreens/OpenSendRecap";
 import CoinStore from "./CoinStore";
 import RemoteConfigsStore from "./RemoteConfigsStore";
 import WalletStore from "./WalletStore";
+import { IWalletConnectSession } from "@walletconnect/types"
+
+const session_location = "wc_sessions"
 
 export default class DappConnectionStore {
 	connections: WalletConnectCosmosClientV1[] = []
+	firstLoadHandler
 
 	loading = {
 	  checkNick: false,
 	};
-  
+
 	async checkNick(nick: string) {
 	  return true;
 	}
 
-	constructor(private walletStore: WalletStore, private coinStore: CoinStore, private remoteConfigsStore: RemoteConfigsStore) {
+	constructor(private walletStore: WalletStore, private coinStore: CoinStore, private remoteConfigsStore: RemoteConfigsStore)
+	{
 		makeAutoObservable(this, {}, { autoBind: true })
+
+		// AsyncStorageLib.removeItem(session_location)
+		this.firstLoadHandler = autorun(() =>
+		{
+			if(this.walletStore.activeWallet)
+			{
+				this.load()
+				this.firstLoadHandler()
+			}
+		})
 	}
 
-	async connect(pairString: string)
+	async load()
 	{
+		try
+		{
+			const storedSessions = await AsyncStorageLib.getItem(session_location)
+			if(storedSessions)
+			{
+				const sessions = JSON.parse(storedSessions) as IWalletConnectSession[]
+				sessions.forEach(session => {
+					this.connect(undefined, session)
+				})
+			}
+		}
+		catch(e)
+		{
+			console.log("load to large", e)
+		}
+		reaction(
+			() => JSON.stringify(this.connections.filter(c => c.connector != undefined).map(c => c.connector?.session)),
+			(raw) =>
+			{
+				try {
+					AsyncStorageLib.setItem(session_location, raw)
+				}
+				catch(e)
+				{
+					console.log("save to large", e)
+				}
+			}
+		)
+	}
+
+	save()
+	{
+		const raw = JSON.stringify(this.connections.filter(c => c.connector != null).map(c => c.connector?.session))
+		console.log(raw)
+		try {
+			AsyncStorageLib.setItem(session_location, raw)
+		}
+		catch(e)
+		{
+			console.log("save to large", e)
+		}
+	}
+
+	async connect(pairString?: string, session?: IWalletConnectSession)
+	{
+		// See if it can be checked between pair string and session
+		// if(!this.uris.find(u => u == pairString)) runInAction(() =>
+		// {
+		// 	this.uris.push(pairString)
+		// })
 		if(this.walletStore.activeWallet)
 		{
 			try
 			{
-				this.connections.push(new WalletConnectCosmosClientV1(pairString,
-					[this.walletStore.activeWallet.wallets.btsg],
-					this.remoteConfigsStore.pushNotificationToken,
-					this.onRequest))
+				this.connections.push(new WalletConnectCosmosClientV1({
+					uri: pairString,
+					session,
+					wallets: [this.walletStore.activeWallet.wallets.btsg],
+					fcmToken: this.remoteConfigsStore.pushNotificationToken,
+					onRequest: this.onRequest,
+					onConnect: this.onConnect,
+					onDisconnect: this.onDisconnect,
+				}))
 			}
 			catch(e)
 			{
@@ -60,5 +131,16 @@ export default class DappConnectionStore {
 				}, handler.reject)
 				break
 		}
+	}
+
+	onConnect(connection: WalletConnectCosmosClientV1)
+	{
+		this.save()
+	}
+
+	onDisconnect(connection: WalletConnectCosmosClientV1)
+	{
+		this.connections.splice(this.connections.indexOf(connection), 1)
+		this.save()
 	}
 }
