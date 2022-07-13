e5db73d343821ab7a44973b2eb1e0ddfd5358f4e
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-07-13 06:45:50 +0200

    build: :arrow_up: upgraded build tools, react and expo version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,4 +5,5 @@ rm -r "$PWD/yarn.lock"
 yarn
 sed -i '/android {/a compileOptions {\n sourceCompatibility JavaVersion.VERSION_1_8 \n targetCompatibility JavaVersion.VERSION_1_8 \n }' "$PWD/node_modules/react-native-keychain/android/build.gradle"
 npx rn-nodeify --install --hack
+find ${PWD}/node_modules/*/ -name "build.gradle" -type f | xargs sed -i 's/\bcompile\b/implementation/g'
 jetify
