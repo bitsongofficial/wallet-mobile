2ba6f2e65701fd9f94bd49cdf8aa2dd55a5f5aba
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-07-15 15:55:36 +0200

    feat: :green_heart: updated script.sh and dependencies to work on mac

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,15 @@ echo "current dir: $PWD"
 rm -r "$PWD/node_modules"
 rm -r "$PWD/yarn.lock"
 yarn
-sed -i '/android {/a compileOptions {\n sourceCompatibility JavaVersion.VERSION_1_8 \n targetCompatibility JavaVersion.VERSION_1_8 \n }' "$PWD/node_modules/react-native-keychain/android/build.gradle"
+sed -i '/android {/a\
+  compileOptions {\
+    sourceCompatibility JavaVersion.VERSION_1_8 \
+    targetCompatibility JavaVersion.VERSION_1_8 \
+  }' "$PWD/node_modules/react-native-keychain/android/build.gradle"
 npx rn-nodeify --install --hack
-find ${PWD}/node_modules/*/ -name "build.gradle" -type f | xargs sed -i 's/\bcompile\b/implementation/g'
+if [[ $OSTYPE == 'darwin'* ]]; then
+	find ${PWD}/node_modules/*/ -name "build.gradle" -type f | xargs sed -i "s/[[:<:]]compile[[:>:]]/implementation/g"
+else
+	find ${PWD}/node_modules/*/ -name "build.gradle" -type f | xargs sed -i 's/\bcompile\b/implementation/g'
+fi
 jetify
