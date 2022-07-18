ccd3208dafe8bdf467b7ac0dd6b3ec91c85789f8
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-07-18 18:52:16 +0300

    feat: 🎨  upd. colors
    add LightGreyBlue PearlBlackberry PaleBlue

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,24 +1,29 @@
 enum COLOR {
-  White = "#FFFFFF",
-  Grey1 = "#6B6B84",
+	White = "#FFFFFF",
+	Grey1 = "#6B6B84",
 
-  Dark1 = "#373764",
-  Dark2 = "#2B2B43",
-  Dark3 = "#14142E",
+	Dark1 = "#373764",
+	Dark2 = "#2B2B43",
+	Dark3 = "#14142E",
 
-  Marengo = "#5A5A6D",
-  RoyalBlue = "#4863E8",
-  RoyalBlue2 = "#575BDB",
-  RoyalBlue3 = "#4C62E5",
-  RoyalBlue4 = "#535DDF",
+	Marengo = "#5A5A6D",
+	RoyalBlue = "#4863E8",
+	RoyalBlue2 = "#575BDB",
+	RoyalBlue3 = "#4C62E5",
+	RoyalBlue4 = "#535DDF",
+	RoyalBlue5 = "#4B62E6",
 
-  Lavender = "#D2D7F7",
-  Purple = "#9493DF",
+	Lavender = "#D2D7F7",
+	Purple = "#9493DF",
 
-  Silver = "#C4C4C4",
+	Silver = "#C4C4C4",
 
-  GreenCrayola = "#12A481",
-  Pink = "#DC004F",
+	GreenCrayola = "#12A481",
+	Pink = "#DC004F",
+
+	LightGreyBlue = "#67BAB0",
+	PearlBlackberry = "#656476",
+	PaleBlue = "#EAEAEB",
 }
 
-export default COLOR;
+export default COLOR
