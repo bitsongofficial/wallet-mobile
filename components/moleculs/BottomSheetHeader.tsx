b88afd05f42d750f84e5477cf5ff1ee53ce0eaa3
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-07-21 19:48:14 +0300

    feat: 🧬 + BottomSheetHeader

---
+++ components/moleculs/BottomSheetHeader.tsx
@@ -0,0 +1,47 @@
+import { StyleProp, StyleSheet, Text, View, ViewStyle } from "react-native"
+import { observer } from "mobx-react-lite"
+import { useTheme } from "hooks"
+
+type Props = {
+	title: string
+	subtitle?: string | boolean
+	Pagination: JSX.Element
+	style?: StyleProp<ViewStyle>
+}
+
+export default observer<Props>(function Header({ Pagination, subtitle, title, style }) {
+	const theme = useTheme()
+	return (
+		<View style={[styles.container, style]}>
+			<View style={styles.row}>
+				<Text style={[styles.title, theme.text.primary]}>{title}</Text>
+				{Pagination}
+			</View>
+			{!!subtitle && <Text style={[styles.caption, theme.text.secondary]}>{subtitle}</Text>}
+		</View>
+	)
+})
+
+const styles = StyleSheet.create({
+	container: {},
+	row: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	title: {
+		fontFamily: "CircularStd",
+		fontStyle: "normal",
+		fontWeight: "500",
+		fontSize: 21,
+	},
+	caption: {
+		fontFamily: "CircularStd",
+		fontStyle: "normal",
+		fontWeight: "400",
+		fontSize: 16,
+		marginTop: 8,
+	},
+	left: {},
+	right: {},
+})
