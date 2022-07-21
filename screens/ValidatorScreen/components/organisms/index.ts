1d830791ac440feb1aa6219b7cf23e65802b55ae
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-07-21 19:43:28 +0300

    feat: 🦠 + StepSetAmount

--- screens/ValidatorScreen/components/organisms/index.ts
+++ screens/ValidatorScreen/components/organisms/index.ts
@@ -1,2 +1,3 @@
 export { default as FooterDelegate } from "./FooterDelegate"
 export { default as StepDelegationRecap } from "./StepDelegationRecap"
+export { default as StepSetAmount } from "./StepSetAmount"
