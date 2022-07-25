8b053081113b86975ff73cd4f0de60eda2cbe88e
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-07-25 03:21:22 +0200

    feat: :sparkles: added submit proposal operation

---
+++ core/coin/cosmos/operations/SubmitProposal.ts
@@ -0,0 +1,48 @@
+import { assertIsDeliverTxSuccess, GasPrice, MsgDepositEncodeObject, MsgSubmitProposalEncodeObject, SigningStargateClient } from "@cosmjs-rn/stargate";
+import { DepositData } from "core/types/coin/cosmos/DepositData";
+import { SubmitProposalData } from "core/types/coin/cosmos/SubmitProposalData";
+import { TextProposal } from "cosmjs-types/cosmos/gov/v1beta1/gov";
+import { MsgDeposit, MsgSubmitProposal } from "cosmjs-types/cosmos/gov/v1beta1/tx";
+import { CosmosOperation } from "./CosmosOperation";
+
+export class SubmitProposal extends CosmosOperation {
+	async Run(data: SubmitProposalData) {
+		const walletInfos = await Promise.all(
+			[
+				data.proposer.Address(),
+				data.proposer.Signer(),
+			])
+		const wallet = walletInfos[1]
+		const client = await SigningStargateClient.connectWithSigner(this.coin.RPCEndpoint(), wallet, {
+			gasPrice: GasPrice.fromString("0.001ubtsg"),
+		})
+		console.log(data)
+		const message: MsgSubmitProposalEncodeObject = {
+			typeUrl: "/cosmos.gov.v1beta1.MsgSubmitProposal",
+			value: MsgSubmitProposal.fromPartial({
+				content: {
+					typeUrl: "/cosmos.gov.v1beta1.TextProposal",
+					value: TextProposal.encode(TextProposal.fromPartial({
+						title: data.proposal.title,
+						description: data.proposal.description,
+					})).finish()
+				},
+				initialDeposit: [data.initialDeposit],
+				proposer: walletInfos[0],
+			})
+		}
+
+		try
+		{
+			const result = await client.signAndBroadcast(walletInfos[0], [message], "auto")
+			assertIsDeliverTxSuccess(result)
+				
+			return true
+		}
+		catch(e)
+		{
+			console.log(e)
+		}
+		return false
+	}
+}
