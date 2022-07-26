2704b533916be1805021458a8cea637f32d7c03c
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-07-26 20:22:21 +0300

    feat: ✨ + ValidatorStore

--- stores/MainStore.ts
+++ stores/MainStore.ts
@@ -1,26 +1,35 @@
-import { User } from "classes";
-import { makeAutoObservable } from "mobx";
-import SettingsStore from "./SettingsStore";
-import WalletStore from "./WalletStore";
-import CoinStore from "./CoinStore";
-import RemoteConfigsStore from "./RemoteConfigsStore";
-import DappConnectionStore from "./DappConnectionStore";
-import LocalStorageManager from "./LocalStorageManager";
-import ContactsStore from "./ContactsStore";
+import { User } from "classes"
+import { makeAutoObservable } from "mobx"
+import SettingsStore from "./SettingsStore"
+import WalletStore from "./WalletStore"
+import CoinStore from "./CoinStore"
+import RemoteConfigsStore from "./RemoteConfigsStore"
+import DappConnectionStore from "./DappConnectionStore"
+import LocalStorageManager from "./LocalStorageManager"
+import ContactsStore from "./ContactsStore"
+import ValidatorStore from "./ValidatorStore"
 
 export default class MainStore {
-  auth = null;
-  configs = {
-    remote: new RemoteConfigsStore(),
-  };
-  settings = new SettingsStore();
-  wallet = new WalletStore(this.settings, this.configs.remote);
-  coin = new CoinStore(this.wallet, this.configs.remote, this.settings);
-  dapp = new DappConnectionStore(this.wallet, this.coin, this.configs.remote, this.settings);
-  contacts = new ContactsStore()
-  localStorageManager = new LocalStorageManager(this.wallet, this.dapp, this.settings, this.configs.remote, this.contacts)
+	auth = null
+	configs = {
+		remote: new RemoteConfigsStore(),
+	}
+	settings = new SettingsStore()
+	wallet = new WalletStore(this.settings, this.configs.remote)
+	coin = new CoinStore(this.wallet, this.configs.remote, this.settings)
+	dapp = new DappConnectionStore(this.wallet, this.coin, this.configs.remote, this.settings)
+	contacts = new ContactsStore()
+	localStorageManager = new LocalStorageManager(
+		this.wallet,
+		this.dapp,
+		this.settings,
+		this.configs.remote,
+		this.contacts,
+	)
 
-  constructor() {
-    makeAutoObservable(this, {}, { autoBind: true });
-  }
+	validator = new ValidatorStore()
+
+	constructor() {
+		makeAutoObservable(this, {}, { autoBind: true })
+	}
 }
