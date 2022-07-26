75f50b16319a0be5cdfba0cb14f6f03557c46855
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-07-26 20:19:23 +0300

    refactor: remove ValidatorScreen's hooks

--- screens/ValidatorScreen/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as useModalDelegate } from "./useModalDelegate"
-export { default as useModalRedelegate } from "./useModalRedelegate"
-export { default as useModalUndelegate } from "./useModalUndelegate"
