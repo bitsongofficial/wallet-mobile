0fe9e69b59305ba0003ab9776f3ae5faf553085c
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-07-26 20:22:59 +0300

    feat(class): ✨ + steps.clear

--- classes/Steps.ts
+++ classes/Steps.ts
@@ -1,44 +1,49 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable } from "mobx"
 
 export default class Steps<T extends string = any> {
-  active = 0;
-  history: number[] = [this.active];
+	active = 0
+	history: number[] = [this.active]
 
-  constructor(public titles: T[]) {
-    makeAutoObservable(this, {}, { autoBind: true });
-  }
+	constructor(public titles: T[]) {
+		makeAutoObservable(this, {}, { autoBind: true })
+	}
 
-  get title() {
-    return this.titles[this.active];
-  }
+	clear() {
+		this.active = 0
+		this.history = [0]
+	}
 
-  private setActive(index?: number) {
-    if (index !== undefined) this.active = index;
-  }
+	get title() {
+		return this.titles[this.active]
+	}
 
-  private add(index: number) {
-    if (index >= 0 && index <= this.titles.length - 1) {
-      this.history.push(index);
-      this.setActive(index);
-    }
-  }
+	private setActive(index?: number) {
+		if (index !== undefined) this.active = index
+	}
 
-  goBack() {
-    this.history.pop();
-    this.setActive(this.history[this.history.length - 1]);
-  }
+	private add(index: number) {
+		if (index >= 0 && index <= this.titles.length - 1) {
+			this.history.push(index)
+			this.setActive(index)
+		}
+	}
 
-  goTo(title: T) {
-    this.add(this.titles.findIndex(Steps.equal(title)));
-  }
+	goBack() {
+		this.history.pop()
+		this.setActive(this.history[this.history.length - 1])
+	}
 
-  next() {
-    this.add(this.active + 1);
-  }
+	goTo(title: T) {
+		this.add(this.titles.findIndex(Steps.equal(title)))
+	}
 
-  prev() {
-    this.add(this.active - 1);
-  }
+	next() {
+		this.add(this.active + 1)
+	}
 
-  static equal = (title: string) => (t: string) => t === title;
+	prev() {
+		this.add(this.active - 1)
+	}
+
+	static equal = (title: string) => (t: string) => t === title
 }
