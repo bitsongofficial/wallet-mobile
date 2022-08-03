ad6f355d054366e23c41dd93867cbb0c6d988b2c
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-08-03 18:31:57 +0300

    refactor: wip upd. conStore

--- stores/index.ts
+++ stores/index.ts
@@ -1,4 +1,4 @@
-export { default as CoinStore } from "./CoinStore";
-export { default as MainStore } from "./MainStore";
-export { default as SettingsStore } from "./SettingsStore";
-export { default as WalletStore } from "./WalletStore";
+export { default as CoinStore } from "./CoinStore_2"
+export { default as MainStore } from "./MainStore"
+export { default as SettingsStore } from "./SettingsStore"
+export { default as WalletStore } from "./WalletStore"
