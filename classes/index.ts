f7e5b6c844066c3bc01f45aaa86c7c068543365f
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-08-03 18:34:20 +0300

    feat(class): wip add new coin class

--- classes/index.ts
+++ classes/index.ts
@@ -1,4 +1,5 @@
 export { default as Coin } from "./Coin"
+export { default as Coin_fixed } from "./Coin_fixed"
 export { default as Biometric } from "./Biometric"
 export { default as Phrase } from "./Phrase"
 export { default as Pin } from "./Pin"
