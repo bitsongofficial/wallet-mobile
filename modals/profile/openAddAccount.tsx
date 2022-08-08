8c3be09c96ab91a69e029023ec0c2a615b27ffff
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-08-08 16:12:50 +0300

    fix(modals): upd. openAddAccount, added controller #30

--- modals/profile/openAddAccount.tsx
+++ modals/profile/openAddAccount.tsx
@@ -1,9 +1,11 @@
-import { Keyboard, Platform } from "react-native"
-import { BottomSheetProps } from "@gorhom/bottom-sheet"
+import { Keyboard } from "react-native"
+import { BottomSheetFooterProps, BottomSheetProps } from "@gorhom/bottom-sheet"
 import { gbs } from "modals"
-import { Phrase, Steps } from "classes"
 import { reaction } from "mobx"
-import { AddAccount } from "./components/organisms"
+import { wait } from "utils"
+import { store } from "stores/Store"
+import { AddAccount, FooterAddAccount } from "./components/organisms"
+import { ControllerAddAccount } from "./controllers"
 
 type Options = {
 	props?: Omit<Partial<BottomSheetProps>, "onClose" | " children">
@@ -12,50 +14,71 @@ type Options = {
 
 export default async function openChangeAvatar({ props, onClose }: Options) {
 	const close = () => {
-		Keyboard.dismiss()
 		gbs.close()
+		wait(400).then(Keyboard.dismiss)
 	}
-	const steps = new Steps(["Choose", "Create", "Name", "Import"])
-	const phrase = new Phrase()
 
-	const disposer = reaction(
-		() => steps.title,
-		(title) => {
-			switch (title) {
-				case "Create":
-					gbs.updProps({ snapPoints: ["95%"] })
-					phrase.create()
-					break
-				case "Import":
-					gbs.updProps({ snapPoints: ["95%"] })
-					phrase.clear()
-					break
-				case "Name":
-					gbs.updProps({ snapPoints: ["95%"] })
-					break
-				default:
-					gbs.updProps({ snapPoints: [350] })
-					break
-			}
-		},
-	)
+	const controller = new ControllerAddAccount()
 
-	const goBack = () => (steps.history.length > 1 ? steps.goBack() : close())
+	const onChangeTitle = (title: string) => {
+		switch (title) {
+			case "Create":
+				gbs.updProps({ snapPoints: ["95%"] })
+				controller.phrase.create()
+				break
+			case "Import":
+				gbs.updProps({ snapPoints: ["95%"] })
+				controller.phrase.clear()
+				break
+			case "Name":
+				gbs.updProps({ snapPoints: ["95%"] })
+				break
+			default:
+				gbs.updProps({ snapPoints: [350] })
+				break
+		}
+	}
+
+	const goBack = () => (controller.steps.history.length > 1 ? controller.steps.goBack() : close())
 
 	gbs.backHandler = () => goBack()
 
-	await gbs.setProps({
-		snapPoints: [350],
-		keyboardBehavior: Platform.OS === "android" ? "interactive" : "fillParent",
-		...props,
-		onChange(index) {
-			if (index === -1) {
-				disposer()
-				gbs.removeBackHandler()
-				onClose && onClose()
-			}
-		},
-		children: () => <AddAccount steps={steps} phrase={phrase} close={close} />,
-	})
-	requestAnimationFrame(() => gbs.expand())
+	const saveWallet = () => {
+		const { nameInput, phrase } = controller
+		const { wallet } = store
+		if (nameInput.value && phrase.isValid) {
+			close()
+			wallet.newCosmosWallet(nameInput.value, phrase.words)
+		}
+	}
+
+	const open = async () => {
+		const disposer = reaction(() => controller.steps.title, onChangeTitle)
+
+		await gbs.setProps({
+			snapPoints: [350],
+			keyboardBehavior: "interactive",
+			...props,
+			onChange(index) {
+				if (index === -1) {
+					disposer()
+					gbs.removeBackHandler()
+					onClose && onClose()
+				}
+			},
+			footerComponent: (props: BottomSheetFooterProps) => (
+				<FooterAddAccount
+					{...props}
+					controller={controller}
+					onPressAddWallet={saveWallet}
+					onPressBack={goBack}
+				/>
+			),
+			children: () => <AddAccount controller={controller} />,
+		})
+
+		requestAnimationFrame(() => gbs.expand())
+	}
+
+	open()
 }
