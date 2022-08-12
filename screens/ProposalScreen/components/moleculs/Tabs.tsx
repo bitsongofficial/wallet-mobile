231f19e898df33f472977657c5e513a8f9d65007
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-08-12 14:37:03 +0300

    feat: 🧬 + Head, Tabs #94

--- screens/ProposalScreen/components/moleculs/Tabs.tsx
+++ screens/ProposalScreen/components/moleculs/Tabs.tsx
@@ -3,9 +3,8 @@ import { StyleProp, StyleSheet, Text, ViewStyle } from "react-native"
 import { RectButton, ScrollView } from "react-native-gesture-handler"
 import { COLOR } from "utils"
 
-export type ITab = "All" | "Deposit" | "Voting" | "Passed"
-
-const tabs: ITab[] = ["All", "Deposit", "Voting", "Passed"]
+export type ITab = "All" | "Deposit" | "Voting" | "Passed" | "Rejected" | "Draft"
+const tabs: ITab[] = ["All", "Deposit", "Voting", "Passed", "Rejected", "Draft"]
 
 type Props = {
 	style: StyleProp<ViewStyle>
@@ -14,9 +13,12 @@ type Props = {
 }
 
 export default memo(({ style, active, onPress }: Props) => (
-	<ScrollView horizontal contentContainerStyle={[styles.content, style]}>
-		{tabs.map((tab) => (
-			<RectButton onPress={() => onPress(tab)} style={[styles.tab, tab !== "Passed" && styles.gap]}>
+	<ScrollView horizontal contentContainerStyle={style}>
+		{tabs.map((tab, index) => (
+			<RectButton
+				onPress={() => onPress(tab)}
+				style={[styles.tab, index !== tabs.length - 1 && styles.gap]}
+			>
 				<Text style={[styles.text, active === tab && styles.active]}>{tab}</Text>
 			</RectButton>
 		))}
@@ -24,10 +26,10 @@ export default memo(({ style, active, onPress }: Props) => (
 ))
 
 const styles = StyleSheet.create({
-	content: { paddingVertical: 10 },
 	tab: {
 		paddingHorizontal: 12,
-		paddingVertical: 5,
+		height: 48,
+		justifyContent: "center",
 	},
 	gap: {
 		marginRight: 30,
--- screens/ProposalScreen/components/moleculs/index.ts
+++ screens/ProposalScreen/components/moleculs/index.ts
@@ -1,3 +1,4 @@
 export { default as Tabs, ITab } from "./Tabs"
 export { default as CardCoin } from "./CardCoin"
+export { default as Head } from "./Head"
 export { default as CardCommission } from "./CardCommission"
