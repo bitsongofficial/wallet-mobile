2b23b47e0a9242a0a62ff404b1ae446b6084f831
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-08-22 13:18:06 +0300

    feat: ✨  upd. NewProposalScreen #94

--- screens/NewProposalScreen/NewProposalScreen.tsx
+++ screens/NewProposalScreen/NewProposalScreen.tsx
@@ -1,56 +1,121 @@
 import { useCallback, useMemo, useState } from "react"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { observer } from "mobx-react-lite"
 import { StatusBar } from "expo-status-bar"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
-import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native"
-import { RootStackParamList, RootTabParamList } from "types"
-import { COLOR, InputHandler } from "utils"
-import { ScrollView } from "react-native-gesture-handler"
-import { Button, Input } from "components/atoms"
+import { KeyboardAvoidingView, Platform, SafeAreaView, StyleSheet, Text, View } from "react-native"
+import { RootStackParamList } from "types"
+import { COLOR, hexAlpha, InputHandler } from "utils"
+import { RectButton, ScrollView } from "react-native-gesture-handler"
+import { Button, Icon2, Input } from "components/atoms"
+import { ViewProps } from "components/Themed"
+import { useHeaderHeight } from "@react-navigation/elements"
+import { openChangeVoteTypology } from "modals/proposal"
 
 type Props = NativeStackScreenProps<RootStackParamList, "Validator">
 
 export default observer<Props>(function Stacking({ navigation }) {
 	const goBack = useCallback(() => navigation.goBack(), [])
 
-	const nameInput = useMemo(() => new InputHandler(), [])
+	const nameInput = useMemo(() => new InputHandler("My super proposal"), [])
+
+	const [typology, setTypology] = useState<"text" | "software">("text")
+	const openChooseProposalTypologyModal = useCallback(
+		() =>
+			openChangeVoteTypology({
+				typology,
+				onChange: setTypology,
+			}),
+		[typology],
+	)
+
+	const inputDeposite = useMemo(() => new InputHandler(), [])
+
+	const height = useHeaderHeight()
 
 	return (
 		<>
 			<StatusBar style="light" />
-
 			<SafeAreaView style={styles.safearea}>
-				<ScrollView style={styles.container}>
-					<Text style={styles.title}>Create New Proposal</Text>
+				<KeyboardAvoidingView
+					behavior={Platform.OS === "ios" ? "padding" : "height"}
+					enabled
+					keyboardVerticalOffset={height}
+					style={styles.flex1}
+				>
+					<ScrollView
+						style={styles.container}
+						bounces={false}
+						contentContainerStyle={[styles.scrollContent]}
+					>
+						<View style={styles.head}>
+							<Text style={styles.title}>Create New Proposal</Text>
+							<Button
+								mode="gradient_border"
+								onPress={goBack}
+								contentContainerStyle={styles.buttonGoBackContent}
+							>
+								<Icon2 name="close" size={16} stroke={COLOR.White} />
+							</Button>
+						</View>
 
-					<Text style={styles.label}>Name</Text>
-					<Input
-						value={nameInput.value}
-						onChangeText={nameInput.set}
-						style={styles.input}
-						placeholder="My super proposal"
-					/>
+						<Text style={styles.label}>Name</Text>
+						<Input
+							value="My super proposal"
+							editable={false}
+							style={styles.inputContainer}
+							inputStyle={styles.input}
+						/>
 
-					<Text style={styles.label}>Typology</Text>
-					<Input style={styles.input} placeholder="Text Proposal" />
+						<Text style={styles.label}>Typology</Text>
 
-					<Text style={styles.label}>Initial Deposit</Text>
-					<Input style={styles.input} placeholder="0" />
+						<RectButton onPress={openChooseProposalTypologyModal}>
+							<View pointerEvents="none">
+								<Input
+									value={typology === "text" ? "Text Proposal" : "Software Update"}
+									editable={false}
+									Right={
+										<InputRightContainer>
+											<Icon2 name="chevron_down_fixed" size={18} stroke={COLOR.White} />
+										</InputRightContainer>
+									}
+									style={styles.inputContainer}
+									inputStyle={styles.input}
+								/>
+							</View>
+						</RectButton>
 
-					<Text style={styles.label}>Text Proposal</Text>
-					<Input
-						multiline
-						numberOfLines={4}
-						style={styles.textarea}
-						inputStyle={styles.textAreaInput}
-						placeholder="Text area..."
-					/>
-				</ScrollView>
+						<Text style={styles.label}>Initial Deposit</Text>
+						<Input
+							placeholder="0"
+							Right={
+								<InputRightContainer>
+									<Text style={styles.coinName}>BTSG</Text>
+								</InputRightContainer>
+							}
+							keyboardAppearance="dark"
+							keyboardType="decimal-pad"
+							value={inputDeposite.value}
+							onChangeText={inputDeposite.set}
+							style={styles.inputContainer}
+							inputStyle={styles.input}
+						/>
 
-				<View style={styles.footer}>
+						<Text style={styles.label}>Text Proposal</Text>
+
+						<Input
+							placeholder="Text area..."
+							multiline
+							numberOfLines={4}
+							textAlignVertical="top"
+							keyboardAppearance="dark"
+							style={styles.textarea}
+							inputStyle={[styles.input, styles.textAreaInput]}
+						/>
+					</ScrollView>
+				</KeyboardAvoidingView>
+				<View style={[styles.footer]}>
 					<Button
-						text="Save as a draft"
+						text="Save draft"
 						mode="fill"
 						textStyle={styles.buttonText}
 						contentContainerStyle={styles.buttonContentFill}
@@ -66,15 +131,36 @@ export default observer<Props>(function Stacking({ navigation }) {
 	)
 })
 
+const InputRightContainer = (props: ViewProps) => (
+	<View {...props} style={[styles.inputRightContainer, props.style]} />
+)
+
 const styles = StyleSheet.create({
 	safearea: {
-		flex: 1,
 		backgroundColor: COLOR.Dark3,
+		flex: 1,
 	},
+	flex1: { flex: 1 },
 	container: {
-		// paddingTop: 40, // for header
-		marginTop: Platform.OS === "ios" ? 30 : 60,
+		backgroundColor: COLOR.Dark3,
+		zIndex: 0,
+		paddingBottom: 8,
+	},
+	scrollContent: {
 		paddingHorizontal: 30,
+	},
+
+	head: {
+		flexDirection: "row",
+		marginBottom: 36,
+		alignItems: "center",
+		justifyContent: "space-between",
+	},
+
+	buttonGoBackContent: {
+		paddingVertical: 13,
+		paddingHorizontal: 13,
+		backgroundColor: COLOR.Dark3,
 	},
 
 	title: {
@@ -84,36 +170,58 @@ const styles = StyleSheet.create({
 		fontSize: 18,
 		lineHeight: 23,
 		color: COLOR.White,
-
-		marginBottom: 36,
 	},
 
+	coinName: {
+		fontFamily: "CircularStd",
+		fontStyle: "normal",
+		fontWeight: "500",
+		fontSize: 12,
+		lineHeight: 15,
+		color: COLOR.RoyalBlue6,
+	},
+
+	// ---------- Inputs -----------
 	label: {
 		fontFamily: "CircularStd",
 		fontStyle: "normal",
 		fontWeight: "500",
 		fontSize: 14,
 		lineHeight: 18,
-		color: COLOR.White,
+		color: hexAlpha(COLOR.White, 30),
 
 		marginBottom: 8,
 	},
-	input: {
+
+	inputContainer: {
 		borderRadius: 20,
 		marginBottom: 20,
+	},
+	input: {
+		paddingHorizontal: 30,
+		paddingVertical: 22,
+		height: 62,
 	},
 	textarea: {
 		height: 166,
 		borderRadius: 20,
+		marginBottom: 8,
 	},
 	textAreaInput: {
-		flex: 1,
+		height: "100%",
+		paddingTop: 22,
+		paddingBottom: 22,
+		paddingHorizontal: 32,
 	},
+
+	// ---------- Footer ------------
 
 	footer: {
 		flexDirection: "row",
 		paddingHorizontal: 30,
 		justifyContent: "space-between",
+		alignItems: "flex-end",
+		marginBottom: 8,
 	},
 
 	buttonText: {
@@ -131,7 +239,14 @@ const styles = StyleSheet.create({
 	},
 
 	buttonContentFill: {
-		paddingHorizontal: 0,
+		paddingHorizontal: 35,
 		paddingVertical: 18,
+	},
+
+	inputRightContainer: {
+		height: "100%",
+		paddingHorizontal: 30,
+		alignItems: "center",
+		flexDirection: "row",
 	},
 })
