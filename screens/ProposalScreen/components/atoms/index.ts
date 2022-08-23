bbba454db797ad794a123d905c2865799f627324
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-08-23 13:41:07 +0300

    refactor: reorder Shadow, useAnimatedFlatlist #96

--- screens/ProposalScreen/components/atoms/index.ts
+++ screens/ProposalScreen/components/atoms/index.ts
@@ -1,2 +1,1 @@
 export { default as ButtonChain } from "./ButtonChain"
-export { default as Shadow } from "./Shadow"
--- screens/ProposalScreen/hook/index.ts
+++ screens/ProposalScreen/hook/index.ts
@@ -1,2 +1,1 @@
-export { default as useAnimateFlatlist } from "./useAnimateFlatlist"
 export { default as useProposalStatusName } from "./useProposalStatusName"
