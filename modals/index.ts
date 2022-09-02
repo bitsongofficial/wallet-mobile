4496b116c8e88358554f68351b132b623ff012c9
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-09-02 12:51:55 +0300

    refactor: reorder initial globalLoading

--- modals/index.ts
+++ modals/index.ts
@@ -1,8 +1,11 @@
 import GlobalAlertView from "./GlobalAlertView"
 import GlobalBottomSheet from "./GlobalBottomSheet"
+import GlobalLoading from "./GlobalLoading"
 
 export { default as GlobalBottomSheet } from "./GlobalBottomSheet"
 export { default as GlobalAlertView } from "./GlobalAlertView"
+export { default as GlobalLoading } from "./GlobalLoading"
 
 export const gbs = new GlobalBottomSheet()
 export const gav = new GlobalAlertView()
+export const globalLoading = new GlobalLoading()
