cd7ce06637ce4e51eaee8b8048c576b41984f612
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-09-05 20:29:52 +0300

    refactor: upd. create/import wallet controllers #43

--- screens/CreateSeed/controllers/CreateWalletController.ts
+++ screens/CreateSeed/controllers/CreateWalletController.ts
@@ -1,51 +1,28 @@
-import { makeAutoObservable } from "mobx";
-import { InputHandler } from "utils";
-import { Pin, Steps, Phrase } from "classes";
+import { makeAutoObservable } from "mobx"
+import { InputHandler } from "utils"
+import { Pin, Steps, Phrase } from "classes"
 
 export default class CreateWalletController {
-  steps = new Steps([
-    "Create New Mnemonic",
-    "Name Your Wallet",
-    "Set PIN",
-    "Confirm PIN",
-    "Choice Auth Method",
-  ]);
+	steps = new Steps([
+		"Create New Mnemonic",
+		"Name Your Wallet",
+		"Set PIN",
+		"Confirm PIN",
+		"Choice Auth Method",
+	])
 
-  phrase = new Phrase();
-  walletName = new InputHandler();
-  pin = new Pin();
-  confirm = new Pin();
+	phrase = new Phrase()
+	walletName = new InputHandler()
+	pin = new Pin()
+	confirm = new Pin()
 
-  isPhraseShown = false;
+	isPhraseShown = false
 
-  constructor() {
-    makeAutoObservable(this, {}, { autoBind: true });
-  }
+	constructor() {
+		makeAutoObservable(this, {}, { autoBind: true })
+	}
 
-  get isConfirmEqual() {
-    return this.confirm.value === this.pin.value;
-  }
-
-  get isCanNext() {
-    switch (this.steps.active) {
-      case 0:
-        return true; //AAA this.biometric.access; or this.isPhraseShown
-      case 1:
-        return this.walletName.value.length > 3;
-      case 2:
-        return this.pin.isValid;
-      case 3:
-        return this.confirm.isValid && this.pin.isValid && this.isConfirmEqual;
-      default:
-        return false;
-    }
-  }
-
-  nextStep() {
-    if (this.isCanNext) this.steps.next();
-  }
-
-  setPhraseShown(value: boolean) {
-    this.isPhraseShown = value;
-  }
+	setPhraseShown(value: boolean) {
+		this.isPhraseShown = value
+	}
 }
--- screens/CreateSeed/controllers/ImportFromSeedController.ts
+++ screens/CreateSeed/controllers/ImportFromSeedController.ts
@@ -1,47 +1,16 @@
-import { makeAutoObservable } from "mobx";
-import { InputHandler } from "utils";
-import { Pin, Steps, Phrase } from "classes";
+import { makeAutoObservable } from "mobx"
+import { InputHandler } from "utils"
+import { Pin, Steps, Phrase } from "classes"
 
 export default class ImportFromSeedController {
-  steps = new Steps([
-    "Import your Mnemonic",
-    "Name Your Wallet",
-    "Set PIN",
-    "Confirm PIN",
-  ]);
+	steps = new Steps(["Import your Mnemonic", "Name Your Wallet", "Set PIN", "Confirm PIN"])
+	phrase = new Phrase()
+	inputWord = new InputHandler()
+	walletName = new InputHandler()
+	pin = new Pin()
+	confirm = new Pin()
 
-  phrase = new Phrase();
-
-  inputWord = new InputHandler();
-  walletName = new InputHandler();
-
-  pin = new Pin();
-  confirm = new Pin();
-
-  constructor() {
-    makeAutoObservable(this, {}, { autoBind: true });
-  }
-
-  get isConfirmEqual() {
-    return this.confirm.value === this.pin.value;
-  }
-
-  get isCanNext() {
-    switch (this.steps.active) {
-      case 0:
-        return true //this.phrase.isValid;
-      case 1:
-        return this.walletName.value.length > 3;
-      case 2:
-        return true //this.pin.isValid;
-      case 3:
-        return true //this.confirm.isValid && this.pin.isValid && this.isConfirmEqual;
-      default:
-        return false;
-    }
-  }
-
-  nextStep() {
-    if (this.isCanNext) this.steps.next();
-  }
+	constructor() {
+		makeAutoObservable(this, {}, { autoBind: true })
+	}
 }
