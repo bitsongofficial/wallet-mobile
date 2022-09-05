8fa9d09c802faf0a316541d92beb71569b2f4ccd
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-09-05 20:27:27 +0300

    fix: ⚛ upd. Subtitle #43

--- screens/CreateSeed/components/atoms/Subtitle.tsx
+++ screens/CreateSeed/components/atoms/Subtitle.tsx
@@ -1,27 +1,25 @@
-import { StyleProp, StyleSheet, Text, TextStyle } from "react-native";
-import { useTheme } from "hooks";
+import { StyleProp, StyleSheet, Text, TextStyle } from "react-native"
+import { useTheme } from "hooks"
+import { COLOR } from "utils"
 
 type Props = {
-  text?: string;
-  children?: React.ReactNode;
-  style?: StyleProp<TextStyle>;
-};
+	text?: string
+	children?: React.ReactNode
+	style?: StyleProp<TextStyle>
+}
 
 export default ({ children, text, style }: Props) => {
-  const theme = useTheme();
-  return (
-    <Text style={[styles.text, theme.text.secondary, style]}>
-      {text || children}
-    </Text>
-  );
-};
+	return <Text style={[styles.text, style]}>{text || children}</Text>
+}
 
 const styles = StyleSheet.create({
-  text: {
-    fontFamily: "CircularStd",
-    fontStyle: "normal",
-    fontWeight: "400",
-    fontSize: 14,
-    lineHeight: 18,
-  },
-});
+	text: {
+		fontFamily: "CircularStd",
+		fontStyle: "normal",
+		fontWeight: "400",
+		fontSize: 14,
+		lineHeight: 18,
+
+		color: COLOR.Marengo,
+	},
+})
