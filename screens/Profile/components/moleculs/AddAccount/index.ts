a9b29c9ec76e23a93bafe36c8f266f78390202bb
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-09-06 15:30:08 +0300

    refactor: ♻️ remove unused code

--- screens/Profile/components/moleculs/AddAccount/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CreateStep } from "./CreateStep";
-export { default as ChooseStep } from "./ChooseStep";
-export { default as InputNameStep } from "./InputNameStep";
-export { default as ImportStep } from "./ImportStep";
