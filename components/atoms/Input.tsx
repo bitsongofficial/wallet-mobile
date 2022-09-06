a2e1b89d39bd5725b63055039f832863e26409aa
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-09-06 17:35:38 +0300

    feat: ⚛ Input + render error message #123

--- components/atoms/Input.tsx
+++ components/atoms/Input.tsx
@@ -11,6 +11,7 @@ import {
 import { TextInput } from "react-native-gesture-handler"
 import { useTheme } from "hooks"
 import { BottomSheetTextInput } from "@gorhom/bottom-sheet"
+import { COLOR } from "utils"
 
 type Props = TextInputProps & {
 	style?: StyleProp<ViewStyle>
@@ -18,11 +19,22 @@ type Props = TextInputProps & {
 	autocomplite?: string | null
 	bottomsheet?: boolean
 	Right?: JSX.Element
+
+	errorMessage?: string | false
 }
 
 const LINE_HEIGHT = 18
+const BORDER_RADIUS = 50
 
-export default ({ inputStyle, style, autocomplite, bottomsheet, Right, ...props }: Props) => {
+export default ({
+	inputStyle,
+	style,
+	autocomplite,
+	bottomsheet,
+	Right,
+	errorMessage,
+	...props
+}: Props) => {
 	const theme = useTheme()
 	const Component = useMemo(() => (bottomsheet ? BottomSheetTextInput : TextInput), [bottomsheet])
 
@@ -36,37 +48,47 @@ export default ({ inputStyle, style, autocomplite, bottomsheet, Right, ...props 
 		[inputStyle?.height],
 	)
 
+	const errorBorder = useMemo<false | ViewStyle>(
+		() =>
+			!!errorMessage && {
+				borderWidth: 1,
+				borderColor: COLOR.Pink3,
+			},
+		[errorMessage],
+	)
+
 	return (
-		<View style={[styles.container, theme.input.container, style]}>
+		<View style={[styles.container, theme.input.container, errorBorder, style]}>
 			{autocomplite && (
 				<Text style={[theme.input.autocomplite, styles.autocomplite, autocomplitPosition]}>
 					{autocomplite}
 				</Text>
 			)}
+
 			<View style={styles.row}>
 				<Component
-					style={[theme.input.component, styles.component, inputStyle]}
+					style={[theme.input.component, styles.input, inputStyle]}
 					placeholderTextColor={theme.input.placeholder}
 					{...props}
 				/>
 				{Right}
 			</View>
+
+			<Text style={styles.error}>{errorMessage}</Text>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
 	container: {
-		borderRadius: 50,
-		overflow: "hidden",
-		backgroundColor: "green",
+		borderRadius: BORDER_RADIUS,
 		width: "100%",
 	},
 	row: {
 		flexDirection: "row",
-		// flex: 1,
+		overflow: "hidden",
 	},
-	component: {
+	input: {
 		fontFamily: "CircularStd",
 		fontStyle: "normal",
 		fontWeight: "400",
@@ -86,5 +108,19 @@ const styles = StyleSheet.create({
 		lineHeight: LINE_HEIGHT,
 		top: 19,
 		left: 25,
+	},
+
+	error: {
+		position: "absolute",
+		bottom: -24,
+		left: 24,
+
+		fontFamily: "CircularStd",
+		fontStyle: "normal",
+		fontWeight: "500",
+		fontSize: 12,
+		lineHeight: 15,
+
+		color: COLOR.Pink2,
 	},
 })
