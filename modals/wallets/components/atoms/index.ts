4d0cb2620a8c82ed237c27b571674b97325e187c
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-09-09 21:03:29 +0300

    feat: ⚛ upd. ContactItem #65

--- modals/wallets/components/atoms/index.ts
+++ modals/wallets/components/atoms/index.ts
@@ -1,4 +1,4 @@
-export { default as Message } from "./Message";
-export { default as User } from "./User";
-export { default as Header } from "./Header";
-export { default as Footer } from "./Footer";
+export { default as Message } from "./Message"
+export { default as Contact } from "./Contact"
+export { default as Header } from "./Header"
+export { default as Footer } from "./Footer"
