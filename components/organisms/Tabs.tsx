dd0b164c80503a555c6db6ffcaf7ce288a6c1308
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-09-12 18:51:57 +0300

    refactor: 🦠 Recap, Tabs. upd. styles #65

--- components/organisms/Tabs.tsx
+++ components/organisms/Tabs.tsx
@@ -1,63 +1,51 @@
-import {
-  StyleProp,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  ViewStyle,
-} from "react-native";
-import ThemedGradient from "components/atoms/ThemedGradient";
-import { COLOR } from "utils";
+import { StyleProp, StyleSheet, Text, TouchableOpacity, View, ViewStyle } from "react-native"
+import ThemedGradient from "components/atoms/ThemedGradient"
+import { COLOR } from "utils"
 
-type Props = {
-  values: string[];
-  active: string;
-  onPress(value: string): void;
-  style?: StyleProp<ViewStyle>;
-};
+type Props<T extends string> = {
+	values: T[]
+	active: T
+	onPress(value: T): void
+	style?: StyleProp<ViewStyle>
+}
 
-export default function Tabs({ active, values, style, onPress }: Props) {
-  return (
-    <View style={[styles.container, style]}>
-      {values.map((value, i) => (
-        <TouchableOpacity key={value + i} onPress={() => onPress(value)}>
-          <View style={styles.tab}>
-            <Text
-              style={[styles.tab_value, value !== active && styles.tab_active]}
-            >
-              {value}
-            </Text>
-            {value === active && <ThemedGradient style={styles.marker} />}
-          </View>
-        </TouchableOpacity>
-      ))}
-    </View>
-  );
+export default function Tabs<T extends string>({ active, values, style, onPress }: Props<T>) {
+	return (
+		<View style={[styles.container, style]}>
+			{values.map((value, i) => (
+				<TouchableOpacity key={value + i} onPress={() => onPress(value)}>
+					<View style={styles.tab}>
+						<Text style={[styles.tab_value, value !== active && styles.tab_not_active]}>
+							{value}
+						</Text>
+						{value === active && <ThemedGradient style={styles.marker} />}
+					</View>
+				</TouchableOpacity>
+			))}
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-  },
-  marker: {
-    borderRadius: 4,
-    height: 9,
-    marginTop: 5,
-  },
+	container: { flexDirection: "row" },
+	marker: {
+		borderRadius: 4,
+		height: 9,
+		marginTop: 5,
+	},
 
-  tab: {
-    marginRight: 30,
-  },
-  tab_value: {
-    fontFamily: "CircularStd",
-    fontStyle: "normal",
-    fontWeight: "400",
-    fontSize: 16,
-    lineHeight: 20,
+	tab: { marginRight: 30 },
+	tab_value: {
+		fontFamily: "CircularStd",
+		fontStyle: "normal",
+		fontWeight: "500",
+		fontSize: 16,
+		lineHeight: 20,
 
-    color: COLOR.White,
-  },
-  tab_active: {
-    opacity: 0.3,
-  },
-});
+		color: COLOR.White,
+	},
+	tab_not_active: {
+		color: COLOR.Marengo,
+		fontWeight: "400",
+	},
+})
