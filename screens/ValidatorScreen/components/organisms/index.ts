707272b3fb90414e8b7b0f9ed61304667a1e850f
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2022-09-16 19:51:15 +0300

    refactor(screen): ♻️ Validator, remove unused code #99

--- screens/ValidatorScreen/components/organisms/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as FooterDelegate } from "./FooterDelegate"
-export { default as StepValidatorSelection } from "./StepValidatorSelection"
-export { default as StepDelegationRecap } from "./StepDelegationRecap"
-export { default as StepSetAmount } from "./StepSetAmount"
--- screens/ValidatorScreen/components/template/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Delegate } from "./Delegate"
-export { default as Redelegate } from "./Redelegate"
-export { default as Undelegate } from "./Undelegate"
