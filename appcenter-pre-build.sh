338405c23b2c8c1e03f4e3e3dcb821c9db992ea0
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-10-17 14:44:04 +0200

    build: :sparkles: pre build script now copies appcenter secret from env

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-yarn setup
+SetAppSecretFromEnvToFile () {
+	value=${!1}
+	if [[ -n "$value" ]]; then
+		sed -i -r "s/\{Your app secret here\}/$value/" $2
+	fi
+}
+
+SetAppSecretFromEnv () {
+	SetAppSecretFromEnvToFile APPCENTERSECRET_IOS "./ios/AppCenter-Config.plist"
+	SetAppSecretFromEnvToFile APPCENTERSECRET_ANDROID "./android/app/src/main/assets/appcenter-config.json"
+}
+
+# yarn setup
 yarn env
-yarn build
+SetAppSecretFromEnv
+# yarn build
