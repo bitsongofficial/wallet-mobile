ef8479f44b27d4fbe49486680316244d51b3c794
EnricoBarbieri1997 <enrico.barbieri.1997@gmail.com>
2022-11-13 14:17:08 +0100

    refactor: :coffin: removed unused imports

--- core/types/coin/cosmos/SubmitProposalData.ts
+++ core/types/coin/cosmos/SubmitProposalData.ts
@@ -1,6 +1,5 @@
 import { CosmosWallet } from "core/storing/Wallet";
 import { Amount } from "../Generic";
-import { Proposal, ProposalType } from "./Proposal";
 
 export interface SubmitProposalData {
 	proposer: CosmosWallet,
