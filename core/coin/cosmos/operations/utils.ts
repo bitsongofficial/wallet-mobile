75c4447d3059fcaa6ae8a0dfa21ac6d2f38272e6
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-11-18 03:52:06 +0100

    refactor: :recycle: refactored utilities functions related to amino types

--- core/coin/cosmos/operations/utils.ts
+++ core/coin/cosmos/operations/utils.ts
@@ -1,14 +1,24 @@
 import { CoinOperationEnum } from "core/types/coin/OperationTypes";
 
-export const AminoTypes: {[k in CoinOperationEnum]?: string} = {
-	[CoinOperationEnum.Send]: "/cosmos.bank.v1beta1.MsgSend",
-	[CoinOperationEnum.Delegate]: "/cosmos.staking.v1beta1.MsgDelegate",
-	[CoinOperationEnum.Redelegate]: "/cosmos.staking.v1beta1.MsgBeginRedelegate",
-	[CoinOperationEnum.Undelegate]: "/cosmos.staking.v1beta1.MsgUndelegate",
-	[CoinOperationEnum.Claim]: "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
-	[CoinOperationEnum.Vote]: "/cosmos.gov.v1beta1.MsgVote",
-	[CoinOperationEnum.SubmitProposal]: "/cosmos.gov.v1beta1.MsgSubmitProposal",
-	[CoinOperationEnum.Deposit]: "/cosmos.gov.v1beta1.MsgDeposit",
+export enum AminoTypes {
+	Send = "/cosmos.bank.v1beta1.MsgSend",
+	Delegate = "/cosmos.staking.v1beta1.MsgDelegate",
+	Undelegate = "/cosmos.staking.v1beta1.MsgUndelegate",
+	Redelegate = "/cosmos.staking.v1beta1.MsgBeginRedelegate",
+	Claim = "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward",
+	Vote = "/cosmos.gov.v1beta1.MsgVote",
+	SubmitProposal = "/cosmos.gov.v1beta1.MsgSubmitProposal",
+	Deposit = "/cosmos.gov.v1beta1.MsgDeposit",
+}
+export const OperationAminoTypesMap: {[k in CoinOperationEnum]?: AminoTypes | string} = {
+	[CoinOperationEnum.Send]: AminoTypes.Send,
+	[CoinOperationEnum.Delegate]: AminoTypes.Delegate,
+	[CoinOperationEnum.Redelegate]: AminoTypes.Redelegate,
+	[CoinOperationEnum.Undelegate]: AminoTypes.Undelegate,
+	[CoinOperationEnum.Claim]: AminoTypes.Claim,
+	[CoinOperationEnum.Vote]: AminoTypes.Vote,
+	[CoinOperationEnum.SubmitProposal]: AminoTypes.SubmitProposal,
+	[CoinOperationEnum.Deposit]: AminoTypes.Deposit,
 	[CoinOperationEnum.Balance]: "",
 	[CoinOperationEnum.Validators]: "",
 	[CoinOperationEnum.History]: "",
@@ -18,5 +28,15 @@ export const AminoTypes: {[k in CoinOperationEnum]?: string} = {
 
 export function operationToAminoType(operation: CoinOperationEnum)
 {
-	return AminoTypes[operation]
+	return OperationAminoTypesMap[operation]
+}
+
+const aminoTypesPrettyNames: {
+	[k in AminoTypes | string]?: string
+} = {
+	[AminoTypes.Send]: "Send",
+}
+export function aminoTypePrettyName(aminoType: AminoTypes | string)
+{
+	return aminoTypesPrettyNames[aminoType] ?? undefined
 }
