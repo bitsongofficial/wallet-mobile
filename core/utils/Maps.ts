bfb9d60207f57b3696343cb17d99960e0144c86f
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-11-21 01:51:24 +0100

    feat: :sparkles: added merge maps utility

--- core/utils/Maps.ts
+++ core/utils/Maps.ts
@@ -2,4 +2,10 @@ export function fromObjectToMap<T extends any>(obj: any)
 {
 	const m = new Map<string, T>()
 	Object.keys(obj).forEach(k => m.set(k, obj[k]))
+	return m
+}
+
+export function mergeMaps<K extends any, T extends any>(map1: Map<K, T>, map2: Map<K, T>)
+{
+	return new Map<K, T>([...map1, ...map2])
 }
