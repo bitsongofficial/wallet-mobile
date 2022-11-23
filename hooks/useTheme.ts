096af5c51fa1314bc5d4c34a8c0d4a0761a7208f
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-11-23 02:00:44 +0100

    feat: :lipstick: updated theme configuration

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -37,6 +37,24 @@ interface ITheme {
     indicator: ViewStyle;
   };
   appBackground: ViewStyle;
+  jsonTheme: {
+    base00: string,
+    base01: string,
+    base02: string,
+    base03: string,
+    base04: string,
+    base05: string,
+    base06: string,
+    base07: string,
+    base08: string,
+    base09: string,
+    base0A: string,
+    base0B: string,
+    base0C: string,
+    base0D: string,
+    base0E: string,
+    base0F: string
+  },
 }
 
 const Theme = {
@@ -85,6 +103,24 @@ const Theme = {
     },
     appBackground: {
       backgroundColor: COLOR.Dark3,
+    },
+    jsonTheme: {
+      base00: COLOR.Dark2,
+      base01: '#383830',
+      base02: '#49483e',
+      base03: '#75715e',
+      base04: '#a59f85',
+      base05: '#f8f8f2',
+      base06: '#f5f4f1',
+      base07: '#f9f8f5',
+      base08: '#f92672',
+      base09: '#fd971f',
+      base0A: '#f4bf75',
+      base0B: '#a6e22e',
+      base0C: '#a1efe4',
+      base0D: '#66d9ef',
+      base0E: '#ae81ff',
+      base0F: '#cc6633'
     },
   } as ITheme,
   light: {} as ITheme,
