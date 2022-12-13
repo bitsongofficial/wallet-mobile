fac004d396f571d46a104430fa4aae55aaea1992
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-12-13 03:56:58 +0100

    fix: added observer functionality to info bar as it was not updating

--- components/organisms/TestnetInfoBar.tsx
+++ components/organisms/TestnetInfoBar.tsx
@@ -4,11 +4,12 @@ import InfoBar from './InfoBar'
 import { useTranslation } from 'react-i18next'
 import { useStore } from 'hooks'
 import { COLOR } from 'utils'
-
-export default function TestnetInfoBar() {
+import { observer } from 'mobx-react-lite'
+const TestnetInfoBar = observer(function TestnetInfoBar() {
 	const { t } = useTranslation()
 	const { settings } = useStore()
 	return (
 		<InfoBar backgroundColor={settings.testnet ? COLOR.Pink5 : COLOR.Pink4} text={settings.testnet ? t("Testnet") : t("Mainnet")}></InfoBar>
 	)
-}
+})
+export default TestnetInfoBar
