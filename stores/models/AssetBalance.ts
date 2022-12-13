01a2781e2e1c295a5ce2db7d9e332761b58432e2
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-12-13 03:52:40 +0100

    feat: :label: added origin chain to AssetBalance type

--- stores/models/AssetBalance.ts
+++ stores/models/AssetBalance.ts
@@ -4,23 +4,26 @@ import { makeAutoObservable } from "mobx";
 
 export interface AssetBalance {
 	denom: AssetIndex,
+	chain: string,
 	balance: number,
 }
 
 export class ObservableAssetBalance implements AssetBalance {
 	denom: AssetIndex
+	chain: string
 	balance: number
 	constructor(balance: AssetBalance)
 	{
+		this.chain = balance.chain
 		this.denom = balance.denom
 		this.balance = balance.balance
 		makeAutoObservable(this)
 	}
 
-	static fromAmount(amount: Amount)
+	static fromChainAmount(chain: string, amount: Amount)
 	{
 		const value = parseFloat(amount.amount)
-		if(value != NaN) return new ObservableAssetBalance({denom: amount.denom, balance: value})
+		if(!Number.isNaN(value)) return new ObservableAssetBalance({chain, denom: amount.denom, balance: value})
 		return undefined
 	}
 }
