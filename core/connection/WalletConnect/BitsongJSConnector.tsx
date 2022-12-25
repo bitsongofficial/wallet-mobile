5ea969ad0ca462bfbfedd1e061a03de7b44ae04d
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-12-25 06:21:58 +0100

    fix: :bug: changed continue on browser text with correct loader

--- core/connection/WalletConnect/BitsongJSConnector.tsx
+++ core/connection/WalletConnect/BitsongJSConnector.tsx
@@ -1,7 +1,5 @@
-import { StdFee, StdSignDoc, AminoMsg } from "@cosmjs-rn/amino";
+import { StdFee, StdSignDoc } from "@cosmjs-rn/amino";
 import { SupportedCoins } from "constants/Coins";
-import { chainIdToChain } from "core/utils/Coin";
-import * as CryptoJS from "crypto-js";
 import openConfirm from "modals/general/openConfirm";
 import { WalletConnectBaseEvents, WalletConnectConnectorV1, WalletConnectOptions, WalletConnectVersionedCallbacks } from "./ConnectorV1";
 import KeplrSignRecap from "modals/walletconnect/keplr/KeplrSignRecap";
@@ -9,8 +7,8 @@ import openModal from "modals/general/openModal";
 import SignMessageError from "modals/walletconnect/SignMessageError";
 import ContinueOnDesktop from "modals/walletconnect/ContinueOnDesktop";
 import { EncodeObject } from "@cosmjs-rn/proto-signing";
-import JSONTree from "react-native-json-tree";
 import SignArbitraryRecap from "modals/walletconnect/bitsongjs/SignArbitraryRecap";
+import { navigate } from "navigation/utils";
 
 export interface BitsongEvents extends WalletConnectBaseEvents {
     sign: WalletConnectVersionedCallbacks,
@@ -50,12 +48,6 @@ export class BitsongJSConnector extends WalletConnectConnectorV1<BitsongEvents> 
 
     SessionRequest(error: Error | null, payload: any)
     {
-        console.log(this.connector, {
-            chainId: 1,
-            accounts: [
-                this.walletInterface.Name
-            ],
-        })
         this.connector?.approveSession({
             chainId: 1,
             accounts: [
@@ -120,40 +112,33 @@ export class BitsongJSConnector extends WalletConnectConnectorV1<BitsongEvents> 
     {
         const {chain, data} = payload.params[0] as {chain: string, data: {signerAddress: string, messages: EncodeObject[], fee: number | StdFee | "auto", memo: string}}
         const {messages, signerAddress, fee, memo} = data
-        const snapPoints = ["70%"]
         if(chain)
         {
             openConfirm({
                 children: <KeplrSignRecap messages={[...messages.map(e => ({type: e.typeUrl, value: e.value}))]}></KeplrSignRecap>,
                 onConfirm: async () =>
                 {
-                    try
-                    {
-                        const signedDoc = await this.walletInterface.SignAndBroadCast(chain as SupportedCoins, messages, fee, memo, signerAddress)
-                        if(signedDoc)
+                    navigate("Loader", {
+                        callback: async () =>
                         {
-                            this.approve(payload, [
-                                signedDoc
-                            ])
-                            openModal({
-                                children: <ContinueOnDesktop></ContinueOnDesktop>,
-                                snapPoints,
-                            })
-                            return
-                        }
-                        else
+                            const res = await this.walletInterface.SignAndBroadCast(chain as SupportedCoins, messages, fee, memo, signerAddress)
+                            if(res)
+                            {
+                                this.approve(payload, [
+                                    res
+                                ])
+                            }
+                            else
+                            {
+                                this.reject(payload, new Error("Error during broadcast"))
+                            }
+                            return res
+                        },
+                        onError: () =>
                         {
-                            throw "Sign failed exception"
+                            this.reject(payload, new Error("Error during broadcast"))
                         }
-                    }
-                    catch(e)
-                    {
-                        console.error("Catched", e)
-                        openModal({
-                            children: <SignMessageError></SignMessageError>,
-                            snapPoints,
-                        })
-                    }
+                    })
                 },
                 onDismiss: () =>
                 {
